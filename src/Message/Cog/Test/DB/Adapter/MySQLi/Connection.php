97cd589c361a8590647f4718b2087a4e2f137244
James Moss <james@message.co.uk>
2012-11-19 20:44:44 +0000

    Moving tests into better location

--- src/Message/Cog/Test/DB/Adapter/MySQLi/Connection.php
+++ src/Message/Cog/Test/DB/Adapter/MySQLi/Connection.php
@@ -2,13 +2,17 @@
 
 namespace Message\Cog\DB\Test\Adapter\MySQLi;
 
+use Message\Cog\DB;
 
 class ConnectionTest extends \PHPUnit_Framework_TestCase
 {
 	
 	public function testCanConnect()
 	{
-		
+		$connection = new DB\Adapter\MySQLi\Connection(array(
+			'user' => 'test',
+			''
+		));
 	}
 
 	public function testQuery()
