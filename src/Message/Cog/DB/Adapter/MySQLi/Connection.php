9c000691cb8412b2551926c11a46fe4f174075b0
James Moss <james@message.co.uk>
2012-11-24 15:29:03 +0000

    Adding support for charsets in connection parameters

--- src/Message/Cog/DB/Adapter/MySQLi/Connection.php
+++ src/Message/Cog/DB/Adapter/MySQLi/Connection.php
@@ -19,13 +19,23 @@ class Connection implements ConnectionInterface
 
 	protected function _connect()
 	{
-		if($this->_handle === null) {
-			$this->_handle = new \MySQLi(
-				$this->_params['host'], 
-				$this->_params['user'], 
-				$this->_params['password'], 
-				$this->_params['db']
-			);
+		// If we've already got a connection handle we don't 
+		// need to connect again
+		if($this->_handle !== null) {
+			return;
+		}
+
+		// Make the connection
+		$this->_handle = new \MySQLi(
+			$this->_params['host'], 
+			$this->_params['user'], 
+			$this->_params['password'], 
+			$this->_params['db']
+		);
+
+		// Set the charset
+		if(isset($this->_params['charset'])) {
+			$mysqli->set_charset($this->_params['charset']);	
 		}
 	}
 
@@ -52,6 +62,11 @@ class Connection implements ConnectionInterface
 		$this->_connect();
 
 		return $this->_handle->error;
+	}
+
+	public function getHandle()
+	{
+		return $this->_handle;
 	}
 
 }
