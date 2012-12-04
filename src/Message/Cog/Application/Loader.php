7d7f69d36210f4e3fd9095fcc8c0d96e52dd74dd
Joe Holdcroft <joe@message.co.uk>
2012-12-04 10:48:00 +0000

    Updating Application\Loader to initialise the context earlier

--- src/Message/Cog/Application/Loader.php
+++ src/Message/Cog/Application/Loader.php
@@ -20,6 +20,7 @@ use Message\Cog\Service\ContainerAwareInterface;
  */
 abstract class Loader
 {
+	protected $_context;
 	protected $_baseDir;
 	protected $_services;
 
@@ -52,6 +53,7 @@ abstract class Loader
 	final public function init()
 	{
 		$this->_loadCog();
+		$this->_setContext();
 		$this->_loadModules();
 
 		return $this;
@@ -68,32 +70,13 @@ abstract class Loader
 	}
 
 	/**
-	 * Get the context class for this request.
+	 * Get the context instance for this request.
 	 *
-	 * This looks at the context set on the `Environment` class and looks for
-	 *
-	 * @return Context\ContextInterface Instance of the Context class
+	 * @return Context\ContextInterface The context instance
 	 */
 	public function getContext()
 	{
-		$context   = $this->_services['environment']->context();
-		$className = 'Message\\Cog\\Application\\Context\\' . ucfirst($context);
-
-		if (!class_exists($className)) {
-			// TODO: Change to Application\Exception\UnknownContextException
-			throw new \RuntimeException(
-				sprintf('Context class not found for context: `%s`', $context)
-			);
-		}
-
-		if (!in_array('Message\Cog\Application\Context\ContextInterface', class_implements($className))) {
-			// TODO: Change to Application\Exception\InvalidContextException
-			throw new \RuntimeException(
-				sprintf('Context class does not implement ContextInterface: `%s`', $context)
-			);
-		}
-
-		return new $className;
+		return $this->_context;
 	}
 
 	/**
@@ -132,6 +115,35 @@ abstract class Loader
 			__DIR__ . '/Bootstrap',
 			'Message\Cog\Application\Bootstrap'
 		)->load();
+	}
+
+	/*
+	 * Set the context class for this request.
+	 *
+	 * This looks at the context set on the `Environment` class and looks for
+	 * a context class matching that name in the `Message\Cog\Application\Context`
+	 * namespace.
+	 */
+	final protected function _setContext()
+	{
+		$context   = $this->_services['environment']->context();
+		$className = 'Message\\Cog\\Application\\Context\\' . ucfirst($context);
+
+		if (!class_exists($className)) {
+			// TODO: Change to Application\Exception\UnknownContextException
+			throw new \RuntimeException(
+				sprintf('Context class not found for context: `%s`', $context)
+			);
+		}
+
+		if (!in_array('Message\Cog\Application\Context\ContextInterface', class_implements($className))) {
+			// TODO: Change to Application\Exception\InvalidContextException
+			throw new \RuntimeException(
+				sprintf('Context class does not implement ContextInterface: `%s`', $context)
+			);
+		}
+
+		return $this->_context = new $className;
 	}
 
 	/**
