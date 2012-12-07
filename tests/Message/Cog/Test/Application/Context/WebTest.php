07a91200b83032939800f5ee8bd03f2362b3b69c
Joe Holdcroft <joe@message.co.uk>
2012-12-07 10:41:10 +0000

    Unit tests for web context loader

---
+++ tests/Message/Cog/Test/Application/Context/WebTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Message\Cog\Test\Application\Context;
+
+use Message\Cog\Application\Context\Web;
+use Message\Cog\Test\Service\FauxContainer;
+use Message\Cog\Test\HTTP\FauxDispatcher;
+
+class WebTest extends \PHPUnit_Framework_TestCase
+{
+	protected $_container;
+	protected $_webContext;
+
+	public function setUp()
+	{
+		$this->_container  = new FauxContainer;
+		$this->_webContext = new Web($this->_container);
+	}
+
+	public function testMasterRequestServiceDefined()
+	{
+		$this->assertTrue($this->_container->isShared('http.request.master'));
+		$this->assertInstanceOf('Message\Cog\HTTP\Request', $this->_container['http.request.master']);
+	}
+
+	public function testRunOutputsSomething()
+	{
+		$dispatcher = $this->getMock(
+			'Message\Cog\HTTP\Dispatcher',
+			array('handle', 'send'),
+			array(),
+			'',
+			false
+		);
+
+		$dispatcher
+			->expects($this->once())
+			->method('handle')
+			->with($this->equalTo($this->_container['http.request.master']))
+			->will($this->returnValue($dispatcher));
+
+		$dispatcher
+			->expects($this->once())
+			->method('send');
+
+		$this->_container['http.dispatcher'] = function() use ($dispatcher) {
+			return $dispatcher;
+		};
+
+		$this->_webContext->run();
+	}
+}
