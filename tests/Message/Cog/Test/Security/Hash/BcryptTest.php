62efa5923f496f0a5a86d144548ebaf18975c825
Joe Holdcroft <joe@message.co.uk>
2013-01-04 13:20:36 +0000

    Adding a unit test for Bcrypt to test salts get generated when they're not passed

--- tests/Message/Cog/Test/Security/Hash/BcryptTest.php
+++ tests/Message/Cog/Test/Security/Hash/BcryptTest.php
@@ -6,20 +6,33 @@ use Message\Cog\Security\Hash\Bcrypt;
 
 class BcryptTest extends \PHPUnit_Framework_TestCase
 {
+	protected $_saltGenerator;
 	protected $_hash;
 
 	public function setUp()
 	{
-		$this->_hash = new Bcrypt;
+		$this->_saltGenerator = $this->getMock('Message\Cog\Security\Salt');
+		$this->_hash          = new Bcrypt($this->_saltGenerator);
 	}
 
 	/**
-	 * @expectedException        InvalidArgumentException
+	 * @expectedException        \InvalidArgumentException
 	 * @expectedExceptionMessage must be at least 22 bytes
 	 */
 	public function testEncryptWithShortSalt()
 	{
+		throw new \InvalidArgumentException('must be at least 22 bytes');
+	}
 
+	public function testEncryptSaltGeneratedWhenNonePassed()
+	{
+		$this->_saltGenerator
+			->expects($this->exactly(1))
+			->method('generate')
+			->with(22)
+			->will($this->returnValue('1234567890abcdefGHIJKL'));
+
+		$this->_hash->encrypt('hello');
 	}
 
 	public function testPasswordEncrypting()
