a46be3e3f30ef1edfdff075eac373b87c2b7c224
Joe Holdcroft <joe@message.co.uk>
2013-01-07 11:06:01 +0000

    - Removing module load failure event as it is not fired - Removing Module Event class, not necessary

--- src/Message/Cog/Module/Event.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Message\Cog\Module;
-
-class Event extends \Message\Cog\Event\Event
-{
-	const MODULE_LOADED      = 'module.%s.load.success';
-	const MODULE_LOAD_FAILED = 'module.%s.load.failure';
-}
