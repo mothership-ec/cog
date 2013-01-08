714b740cdda8d8752d49014a962365274b72d987
Joe Holdcroft <joe@message.co.uk>
2013-01-08 12:56:15 +0000

    Standards tweaks

--- src/Message/Cog/Cache/Filesystem.php
+++ src/Message/Cog/Cache/Filesystem.php
@@ -2,15 +2,15 @@
 
 namespace Message\Cog\Cache;
 
-use \TreasureChest\Cache\Filesystem as TCFilesystem;
+use TreasureChest\Cache\Filesystem as TCFilesystem;
 
 /**
-* 
+*
 */
-class Filesystem extends TCFilesystem implements CacheInterface;
+class Filesystem extends TCFilesystem implements CacheInterface
 {
 	public function clear()
 	{
-		return (bool)array_map('unlink', glob($this->path.'*'));
+		return (bool) array_map('unlink', glob($this->path . '*'));
 	}
 }
