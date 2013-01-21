02013deb26b0197bbb80a982acae3b645144f8be
Joe Holdcroft <joe@message.co.uk>
2013-01-21 12:01:41 +0000

    Adding functionality to Functions\Iterable::toObject to pass a class name to use

--- src/Message/Cog/Functions/Iterable.php
+++ src/Message/Cog/Functions/Iterable.php
@@ -11,17 +11,20 @@ class Iterable
 	 *
 	 * @param array $array                  The array to convert
 	 * @param bool  $maintainNumericIndices If true, numeric indecies are retained
+	 * @param string|null $className        Class to create for the returned object,
+	 *                                      if null, stdClass is used
 	 *
-	 * @return stdClass                     The array in object form
+	 * @return object                       The array as an object instance
 	 */
-	public static function toObject($array, $maintainNumericIndices = false)
+	public static function toObject($array, $maintainNumericIndices = false, $className = null)
 	{
 		if (!is_array($array)) {
 			return $array;
 		}
 
+		$className         = $className ?: 'stdClass';
 		$hasNumericIndices = (array_values($array) === $array) && $maintainNumericIndices;
-		$new = $hasNumericIndices ? array() : new stdClass;
+		$new               = $hasNumericIndices ? array() : new $className;
 
 		foreach ($array as $key => $value) {
 			if (is_array($value)) {
