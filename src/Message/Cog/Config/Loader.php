58448f2f42144629c1676e607bdceb9fc96651d6
Joe Holdcroft <joe@message.co.uk>
2013-01-22 17:06:25 +0000

    Finishing off Config\Loader

--- src/Message/Cog/Config/Loader.php
+++ src/Message/Cog/Config/Loader.php
@@ -2,89 +2,83 @@
 
 namespace Message\Cog\Config;
 
+use DirectoryIterator;
+
+/**
+ * Configuration loader.
+ *
+ * Responsible for loading configuration files and adding the compiled result
+ * to the service container.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
 class Loader
 {
 	protected $_services;
-	protected $_cache;
 	protected $_env;
 
-	public function __construct(ContainerInterface $services, CacheInterface $cache, Environment $env)
+	/**
+	 * Constructor.
+	 *
+	 * @param ContainerInterface $services The service container to add configs to
+	 * @param Environment        $env      The environment object
+	 */
+	public function __construct(ContainerInterface $services, Environment $env)
 	{
 		$this->_services = $services;
-		$this->_cache    = $cache;
 		$this->_env      = $env;
 	}
 
 	public function loadFromDirectory($dir)
 	{
-		if (!file_exists($dir)) {
-			throw new Exception\ConfigUnreadableException(sprintf('Config directory `%s` does not exist', $dir));
-		}
+		$dir = rtrim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 
-		if (!is_readable($dir)) {
-			throw new Exception\ConfigUnreadableException(sprintf('Config directory `%s` is not readable'));
-		}
+		$configs = array();
+		$dirs    = array(
+			$dir,
+			$dir . $this->_env->get() . '/',
+			$dir . $this->_env->get() . '/' . $this->_env->getInstallationName() . '/',
+		);
 
-		try {
-			return $this->loadFromCache('cache.' . md5($dir));
-		}
-		catch (Exception $e) {
-			// load from directory
-		}
-
-		if ($this->_cache->exists($cacheKey)) {
-			$this->loadFromCache($cacheKey);
-		}
-
-		# CACHE KEY: config.md5($directory)
-
-		// get overwrite files for current environment
-
-		// get overwrite files for current env&installation
-
-		// loop through files in root directory
-			// grab overwrite for current env if exists
-			// grab overwrite for current env&installation if exists
-			//
-			// for each yaml file, look for env folder version of the config
-			// and also an installation folder
-		$compiler = new Compiler('merchant'); // second argument to not cache?
-		$compiler->add($yamlDataFromBase);
-		$compiler->add($yamlDataFromEnvDir);
-		$compiler->add($yamlDataFromEnvInstallationDir);
-
-		$group = $compiler->compile();
-	}
-
-	public function loadFromCache($key)
-	{
-		if (!$this->_cache->exists($key)) {
-			throw new Exception(sprintf('Config cache `%s` does not exist', $key));
-		}
-
-		$result = $this->_cache->fetch($key);
-
-		try {
-			if (!is_array($result)) {
-				throw new Exception\CacheInvalidException(sprintf('Config cache `%s` is not an array', $key));
+		foreach ($dirs as $key => $dir) {
+			if (!file_exists($dir)) {
+				// Only throw exception if base config directory does not exist
+				if (0 === $key) {
+					throw new Exception(sprintf('Config directory `%s` does not exist', $dir));
+				}
+				continue;
 			}
 
-			foreach ($result as $name => $group) {
-				if (!$name) {
-					throw new Exception\CacheInvalidException(sprintf('Config cache `%s` has empty group key(s)', $key));
-				}
-				if (!$group instanceof Group) {
-					throw new Exception\CacheInvalidException(sprintf('Config cache `%s` group `%s` value was not a valid Group instance', $key, $name));
+			if (!is_readable($dir)) {
+				throw new Exception(sprintf('Config directory `%s` is not readable', $dir));
+			}
+
+			$dir = new DirectoryIterator($dir);
+
+			foreach ($dir as $file) {
+				if ('yml' !== $file->getExtension()) {
+					continue;
 				}
 
-				$this->_addService($name, $group);
+				if (!$file->isReadable()) {
+					throw new Exception(sprintf('Config file `%s` is not readable', $file->getPathname()));
+				}
+
+				$name = 'cfg.' . $file->getBasename('.yml');
+
+				if (!isset($configs[$name])) {
+					$configs[$name] = new Compiler;
+				}
+
+				$configs[$name]->add(file_get_contents($file->getPathname()));
 			}
 		}
-		catch (Exception\CacheInvalidException $e) {
-			$this->_cache->delete($key);
 
-			throw new Exception\CacheInvalidException(sprintf('Config cache `%s` is invalid', $key), null, $e);
+		foreach ($configs as $name => $config) {
+			$this->_addService($name, $config->compile());
 		}
+
+		return true;
 	}
 
 	protected function _addService($name, Group $config)
