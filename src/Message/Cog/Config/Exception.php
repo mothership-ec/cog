bca58e2366a6dd03b3d3ddf4f59b197f5b782599
Joe Holdcroft <joe@message.co.uk>
2013-01-23 16:30:39 +0000

    Documentation

--- src/Message/Cog/Config/Exception.php
+++ src/Message/Cog/Config/Exception.php
@@ -2,6 +2,11 @@
 
 namespace Message\Cog\Config;
 
+/**
+ * Configuration component exception.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
 class Exception extends \Exception
 {
 
--- src/Message/Cog/Config/LoaderInterface.php
+++ src/Message/Cog/Config/LoaderInterface.php
@@ -2,7 +2,18 @@
 
 namespace Message\Cog\Config;
 
+/**
+ * Configuration loader interface.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
 interface LoaderInterface
 {
+	/**
+	 * Load configuration groups and add them to the passed registry.
+	 *
+	 * @param  Registry $registry The registry to add configuration groups to
+	 * @return Registry $registry The same registry is returned
+	 */
 	public function load(Registry $registry);
 }
