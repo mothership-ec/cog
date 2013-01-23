15f8afb8f78d0887316e1f913ce9f728e35ecd45
Joe Holdcroft <joe@message.co.uk>
2013-01-23 16:01:50 +0000

    Documenting & moving directory check in Config\Loader

--- src/Message/Cog/Config/Loader.php
+++ src/Message/Cog/Config/Loader.php
@@ -29,14 +29,36 @@ class Loader implements LoaderInterface
 	 * @param string             $dir      Directory to load configs from
 	 * @param ContainerInterface $services The service container to add configs to
 	 * @param Environment        $env      The environment object
+	 *
+	 * @throws Exception                   If the directory does not exist
 	 */
 	public function __construct($dir, ContainerInterface $services, Environment $env)
 	{
 		$this->_dir      = rtrim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 		$this->_services = $services;
 		$this->_env      = $env;
+
+		if (!file_exists($this->_dir)) {
+			throw new Exception(sprintf('Config directory `%s` does not exist', $this->_dir));
+		}
 	}
 
+	/**
+	 * Load configuration files from the directory.
+	 *
+	 * The base configuration files and any overwrite files for each
+	 * configuration group are passed to the `Compiler`, and the compiled
+	 * configuration `Group` instances are added to the supplied `Registry`
+	 * instance.
+	 *
+	 * @param  Registry $registry The configuration registry to add compiled
+	 *                            configurations to
+	 * @return Registry           The same registry is returned
+	 *
+	 * @throws Exception          If any of the configuration directories are
+	 *                            not readable
+	 * @throws Exception          If any of the configuration files are not readable
+	 */
 	public function load(Registry $registry)
 	{
 		$configs = array();
@@ -48,10 +70,6 @@ class Loader implements LoaderInterface
 
 		foreach ($dirs as $key => $dir) {
 			if (!file_exists($dir)) {
-				// Only throw exception if base config directory does not exist
-				if (0 === $key) {
-					throw new Exception(sprintf('Config directory `%s` does not exist', $dir));
-				}
 				continue;
 			}
 
