854906c40dbfd59122945ec820e58e928ded7abe
Joe Holdcroft <joe@message.co.uk>
2013-01-23 16:25:55 +0000

    Updating LoaderCache to mirror Loader

--- src/Message/Cog/Config/LoaderCache.php
+++ src/Message/Cog/Config/LoaderCache.php
@@ -19,65 +19,92 @@ class LoaderCache extends Loader
 	const CACHE_KEY_PATTERN = 'cache.%s';
 
 	protected $_cache;
+	protected $_cacheKey;
 
 	/**
 	 * Constructor.
 	 *
+	 * @param string             $dir      Directory to load configs from
 	 * @param ContainerInterface $services The service container to add configs to
 	 * @param Environment        $env      The environment object
 	 * @param CacheInterface     $cache    The caching engine to use
 	 */
-	public function __construct(ContainerInterface $services, Environment $env, CacheInterface $cache)
+	public function __construct($dir, ContainerInterface $services, Environment $env, CacheInterface $cache)
 	{
-		parent::__construct($services, $env);
+		parent::__construct($dir, $services, $env);
 
-		$this->_cache = $cache;
+		$this->_cache    = $cache;
+		$this->_cacheKey = sprintf(self::CACHE_KEY_PATTERN, md5($this->_dir));
 	}
 
-	public function loadFromDirectory($dir)
+	/**
+	 * Load the configuration files.
+	 *
+	 * If `loadByCache` doesn't return false, then the registry is returned
+	 * straight away. This would mean the configuration groups were loaded from
+	 * the cache.
+	 *
+	 * If the configuration groups could not be loaded from the cache, then
+	 * `load()` on the parent class is called, loading and compiling the
+	 * configuration groups from the filesystem. They are then cached for speedy
+	 * retrieval next time.
+	 *
+	 * @see loadByCache
+	 * @see parent::load()
+	 *
+	 * @param  Registry $registry The configuration registry to add compiled
+	 *                            configurations to
+	 * @return Registry           The same registry is returned
+	 */
+	public function load(Registry $registry)
 	{
-		// todo: below line is duplicated... make it not!
-		$dir = rtrim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
-
-		$cacheKey = sprintf(self::CACHE_KEY_PATTERN, md5($dir));
-
-		if ($this->loadFromCache($cacheKey)) {
-			return true;
+		if ($this->loadFromCache($registry)) {
+			return $registry;
 		}
 
-		$return = parent::loadFromDirectory($dir);
+		$return = parent::load($registry);
 
-		$this->_cache->store($cacheKey, $return);
+		$this->_cache->store($this->_cacheKey, $registry->getAll());
 
 		return $return;
 	}
 
-	public function loadFromCache($key)
+	/**
+	 * Try to load the configuration groups from the cache.
+	 *
+	 * If the configuration groups are found in the cache, but they are invalid
+	 * in any way, then they are cleared from the cache.
+	 *
+	 * @param  Registry $registry The configuration registry to add configurations to
+	 * @return boolean            True if the configurations could be loaded
+	 *                            from the cache
+	 */
+	public function loadFromCache(Registry $registry)
 	{
-		if (!$this->_cache->exists($key)) {
+		if (!$this->_cache->exists($this->_cacheKey)) {
 			return false;
 		}
 
-		$result = $this->_cache->fetch($key);
+		$result = $this->_cache->fetch($this->_cacheKey);
 
 		try {
 			if (!is_array($result)) {
-				throw new Exception(sprintf('Config cache `%s` is not an array', $key));
+				throw new Exception(sprintf('Config cache `%s` is not an array', $this->_cacheKey));
 			}
 
 			foreach ($result as $name => $group) {
 				if (!$name) {
-					throw new Exception(sprintf('Config cache `%s` has empty group key(s)', $key));
+					throw new Exception(sprintf('Config cache `%s` has empty group key(s)', $this->_cacheKey));
 				}
 				if (!$group instanceof Group) {
-					throw new Exception(sprintf('Config cache `%s` group `%s` value was not a valid Group instance', $key, $name));
+					throw new Exception(sprintf('Config cache `%s` group `%s` value was not a valid Group instance', $this->_cacheKey, $name));
 				}
 
-				$this->_addService($name, $group);
+				$registry->$name = $group;
 			}
 		}
 		catch (Exception $e) {
-			$this->_cache->delete($key);
+			$this->_cache->delete($this->_cacheKey);
 
 			return false;
 		}
