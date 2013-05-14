fefc5780ef60855f3a8842d6e9275c27e76b92fe
Danny Hannah <dannyhannah87@gmail.com>
2013-05-14 12:14:55 +0100

    Make betterer

--- tests/Message/Cog/Test/ValueObject/DateRangeTest.php
+++ tests/Message/Cog/Test/ValueObject/DateRangeTest.php
@@ -12,56 +12,48 @@ class DateRangeTest extends \PHPUnit_Framework_TestCase
 	{
 		// Set the to time
 		$to = DateTime::createFromFormat('d/m/Y H:i:s', '15/05/2013 00:00:00');
-
 		$from = DateTime::createFromFormat('d/m/Y H:i:s', '01/05/2013 00:00:00');
-
 		$dateRange = new DateRange($from, $to);
-		
-		// Set test datetime as today
-		$testDateRange = DateTime::createFromFormat('d/m/Y H:i:s', '14/05/2013 00:00:00');
 
+		// Set test datetime as today
 		// Check that today is before tomorrow
-		$this->assertTrue($dateRange->isInRange($testDateRange));
-		
+		$this->assertTrue($dateRange->isInRange(DateTime::createFromFormat('d/m/Y H:i:s', '14/05/2013 00:00:00')));
+
 		// Set date to more than the to date
-		$testDateRange = DateTime::createFromFormat('d/m/Y H:i:s', '16/05/2013 00:00:01');
-		
 		// Should return false
-		$this->assertFalse($dateRange->isInRange($testDateRange));		
+		$this->assertFalse($dateRange->isInRange(DateTime::createFromFormat('d/m/Y H:i:s', '16/05/2013 00:00:01')));
+
+		// Reset the to and from
 		$to = new DateTime;
 		$to->setTimestamp(strtotime('+1 day'));
-		
 		$from = new DateTime;
 		$from->setTimestamp(strtotime('-1 day'));
-		
 		$dateRange = new DateRange($from, $to);
-		
+
 		// Pass through null, should use current datetime
 		$this->assertTrue($dateRange->isInRange());
-		
+
 		// Check for differnet times
 		$to = DateTime::createFromFormat('d/m/Y H:i:s', '15/05/2013 09:13:52');
-		
 		$from = DateTime::createFromFormat('d/m/Y H:i:s', '15/05/2013 18:13:22');
-		
 		$dateRange = new DateRange($from, $to);
 
 		// Check that the different times work as expected
 		// Should return true
 		$dateRange->assertTrue($dateRange->isInRange(DateTime::createFromFormat('d/m/Y H:i:s', '15/05/2013 10:13:22')));
-		
+
 		// Should return false
 		$dateRange->assertFalse($dateRange->isInRange(DateTime::createFromFormat('d/m/Y H:i:s', '15/05/2013 09:59:99')));
-				
+
 	}
-	
+
 	public function testGetIntervalToEnd()
 	{
-		
+
 	}
 
 	public function getIntervalToStart()
 	{
-		
+
 	}
 }
