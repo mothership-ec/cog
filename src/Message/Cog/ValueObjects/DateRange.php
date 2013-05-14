a7f1294038ec4185e265800d58dc403ccd65efd7
Joe Holdcroft <joe@message.co.uk>
2013-05-14 09:55:23 +0100

    Starting ValueObjects

---
+++ src/Message/Cog/ValueObjects/DateRange.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Message\Cog\ValueObjects;
+
+use DateTime;
+
+class DateRange
+{
+	public function __construct(DateTime $from = null, DateTime $to = null)
+	{
+
+	}
+
+	public function isInRange(DateTime $datetime = null)
+	{
+
+	}
+
+	public function getPeriodToEnd(\DateTime $datetime = null)
+	{
+
+	}
+
+	public function getPeriodToEnd(\DateTime $datetime = null)
+	{
+
+	}
+}
---
+++ src/Message/Cog/ValueObjects/readme.md
@@ -0,0 +1,9 @@
+# Value Objects
+
+This component contains some standard value objects that are likely to be used in most web applications using Cog.
+
+## `Authorship`
+
+## `Money`
+
+## `DateRange`
