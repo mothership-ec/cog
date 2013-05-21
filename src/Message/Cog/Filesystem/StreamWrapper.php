e2345b7ba17b22f22b75c00cbebcc45d5550c918
James Moss <james@message.co.uk>
2013-05-21 11:16:33 +0100

    Tidying up layout of class

--- src/Message/Cog/Filesystem/StreamWrapper.php
+++ src/Message/Cog/Filesystem/StreamWrapper.php
@@ -32,16 +32,16 @@ class StreamWrapper implements StreamWrapperInterface {
 	* Support for fopen(), file_get_contents(), file_put_contents() etc.
 	*
 	* @param string $uri
-	*   A string containing the URI to the file to open.
+	*	 A string containing the URI to the file to open.
 	* @param int $mode
-	*   The file mode ("r", "wb" etc.).
+	*	 The file mode ("r", "wb" etc.).
 	* @param int $options
-	*   A bit mask of STREAM_USE_PATH and STREAM_REPORT_ERRORS.
+	*	 A bit mask of STREAM_USE_PATH and STREAM_REPORT_ERRORS.
 	* @param string $opened_path
-	*   A string containing the path actually opened.
+	*	 A string containing the path actually opened.
 	*
 	* @return bool
-	*   Returns TRUE if file was opened successfully.
+	*	 Returns TRUE if file was opened successfully.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-open.php
 	*/
@@ -69,7 +69,7 @@ class StreamWrapper implements StreamWrapperInterface {
 		$this->handle = ($options & STREAM_REPORT_ERRORS) ? fopen($path, $mode) : @fopen($path, $mode);
 
 		if ((bool) $this->handle && $options & STREAM_USE_PATH) {
-		  $opened_path = $path;
+			$opened_path = $path;
 		}
 
 		return (bool) $this->handle;
@@ -93,15 +93,15 @@ class StreamWrapper implements StreamWrapperInterface {
 	* Support for flock().
 	*
 	* @param int $operation
-	*   One of the following:
-	*   - LOCK_SH to acquire a shared lock (reader).
-	*   - LOCK_EX to acquire an exclusive lock (writer).
-	*   - LOCK_UN to release a lock (shared or exclusive).
-	*   - LOCK_NB if you don't want flock() to block while locking (not
-	*     supported on Windows).
+	*	 One of the following:
+	*	 - LOCK_SH to acquire a shared lock (reader).
+	*	 - LOCK_EX to acquire an exclusive lock (writer).
+	*	 - LOCK_UN to release a lock (shared or exclusive).
+	*	 - LOCK_NB if you don't want flock() to block while locking (not
+	*		 supported on Windows).
 	*
 	* @return bool
-	*   Always returns TRUE at the present time.
+	*	 Always returns TRUE at the present time.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-lock.php
 	*/
@@ -118,10 +118,10 @@ class StreamWrapper implements StreamWrapperInterface {
 	* Support for fread(), file_get_contents() etc.
 	*
 	* @param int $count
-	*   Maximum number of bytes to be read.
+	*	 Maximum number of bytes to be read.
 	*
 	* @return string|bool
-	*   The string that was read, or FALSE in case of an error.
+	*	 The string that was read, or FALSE in case of an error.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-read.php
 	*/
@@ -134,144 +134,154 @@ class StreamWrapper implements StreamWrapperInterface {
 	* Support for fwrite(), file_put_contents() etc.
 	*
 	* @param string $data
-	*   The string to be written.
+	*	 The string to be written.
 	*
 	* @return int
-	*   The number of bytes written.
+	*	 The number of bytes written.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-write.php
 	*/
-	public function stream_write($data) {
-	return fwrite($this->handle, $data);
+	public function stream_write($data)
+	{
+		return fwrite($this->handle, $data);
 	}
 
 	/**
 	* Support for feof().
 	*
 	* @return bool
-	*   TRUE if end-of-file has been reached.
+	*	 TRUE if end-of-file has been reached.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-eof.php
 	*/
-	public function stream_eof() {
-	return feof($this->handle);
+	public function stream_eof()
+	{
+		return feof($this->handle);
 	}
 
 	/**
 	* Support for fseek().
 	*
 	* @param int $offset
-	*   The byte offset to got to.
+	*	 The byte offset to got to.
 	* @param int $whence
-	*   SEEK_SET, SEEK_CUR, or SEEK_END.
+	*	 SEEK_SET, SEEK_CUR, or SEEK_END.
 	*
 	* @return bool
-	*   TRUE on success.
+	*	 TRUE on success.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-seek.php
 	*/
-	public function stream_seek($offset, $whence) {
-	// fseek returns 0 on success and -1 on a failure.
-	// stream_seek   1 on success and  0 on a failure.
-	return !fseek($this->handle, $offset, $whence);
+	public function stream_seek($offset, $whence)
+	{
+		// fseek returns 0 on success and -1 on a failure.
+		// stream_seek	 1 on success and	0 on a failure.
+		return !fseek($this->handle, $offset, $whence);
 	}
 
 	/**
 	* Support for fflush().
 	*
 	* @return bool
-	*   TRUE if data was successfully stored (or there was no data to store).
+	*	 TRUE if data was successfully stored (or there was no data to store).
 	*
 	* @see http://php.net/manual/streamwrapper.stream-flush.php
 	*/
-	public function stream_flush() {
-	return fflush($this->handle);
+	public function stream_flush()
+	{
+		return fflush($this->handle);
 	}
 
 	/**
 	* Support for ftell().
 	*
 	* @return bool
-	*   The current offset in bytes from the beginning of file.
+	*	 The current offset in bytes from the beginning of file.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-tell.php
 	*/
-	public function stream_tell() {
-	return ftell($this->handle);
+	public function stream_tell()
+	{
+		return ftell($this->handle);
 	}
 
 	/**
 	* Support for fstat().
 	*
 	* @return bool
-	*   An array with file status, or FALSE in case of an error - see fstat()
-	*   for a description of this array.
+	*	 An array with file status, or FALSE in case of an error - see fstat()
+	*	 for a description of this array.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-stat.php
 	*/
-	public function stream_stat() {
-	return fstat($this->handle);
+	public function stream_stat()
+	{
+		return fstat($this->handle);
 	}
 
 	/**
 	* Support for fclose().
 	*
 	* @return bool
-	*   TRUE if stream was successfully closed.
+	*	 TRUE if stream was successfully closed.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-close.php
 	*/
-	public function stream_close() {
-	return fclose($this->handle);
+	public function stream_close()
+	{
+		return fclose($this->handle);
 	}
 
 	/**
 	* Gets the underlying stream resource for stream_select().
 	*
 	* @param int $cast_as
-	*   Can be STREAM_CAST_FOR_SELECT or STREAM_CAST_AS_STREAM.
+	*	 Can be STREAM_CAST_FOR_SELECT or STREAM_CAST_AS_STREAM.
 	*
 	* @return resource|false
-	*   The underlying stream resource or FALSE if stream_select() is not
-	*   supported.
+	*	 The underlying stream resource or FALSE if stream_select() is not
+	*	 supported.
 	*
 	* @see http://php.net/manual/streamwrapper.stream-cast.php
 	*/
-	public function stream_cast($cast_as) {
-	return false;
+	public function stream_cast($cast_as)
+	{
+		return false;
 	}
 
 	/**
 	* Support for unlink().
 	*
 	* @param string $uri
-	*   A string containing the URI to the resource to delete.
+	*	 A string containing the URI to the resource to delete.
 	*
 	* @return bool
-	*   TRUE if resource was successfully deleted.
+	*	 TRUE if resource was successfully deleted.
 	*
 	* @see http://php.net/manual/streamwrapper.unlink.php
 	*/
-	public function unlink($uri) {
-	$this->uri = $uri;
-	return drupal_unlink($this->getLocalPath());
+	public function unlink($uri)
+	{
+		$this->uri = $uri;
+		return drupal_unlink($this->getLocalPath());
 	}
 
 	/**
 	* Support for rename().
 	*
 	* @param string $from_uri,
-	*   The URI to the file to rename.
+	*	 The URI to the file to rename.
 	* @param string $to_uri
-	*   The new URI for file.
+	*	 The new URI for file.
 	*
 	* @return bool
-	*   TRUE if file was successfully renamed.
+	*	 TRUE if file was successfully renamed.
 	*
 	* @see http://php.net/manual/streamwrapper.rename.php
 	*/
-	public function rename($from_uri, $to_uri) {
-	return rename($this->getLocalPath($from_uri), $this->getLocalPath($to_uri));
+	public function rename($from_uri, $to_uri)
+	{
+		return rename($this->getLocalPath($from_uri), $this->getLocalPath($to_uri));
 	}
 
 	/**
@@ -282,169 +292,177 @@ class StreamWrapper implements StreamWrapperInterface {
 	* wrappers.
 	*
 	* @param string $uri
-	*   A URI or path.
+	*	 A URI or path.
 	*
 	* @return string
-	*   A string containing the directory name.
+	*	 A string containing the directory name.
 	*
 	* @see drupal_dirname()
 	*/
-	public function dirname($uri = NULL) {
-	list($scheme, $target) = explode('://', $uri, 2);
-	$target  = $this->getTarget($uri);
-	$dirname = dirname($target);
+	public function dirname($uri = NULL)
+	{
+		list($scheme, $target) = explode('://', $uri, 2);
+		$target	= $this->getTarget($uri);
+		$dirname = dirname($target);
 
-	if ($dirname == '.') {
-	  $dirname = '';
-	}
+		if ($dirname == '.') {
+			$dirname = '';
+		}
 
-	return $scheme . '://' . $dirname;
+		return $scheme . '://' . $dirname;
 	}
 
 	/**
 	* Support for mkdir().
 	*
 	* @param string $uri
-	*   A string containing the URI to the directory to create.
+	*	 A string containing the URI to the directory to create.
 	* @param int $mode
-	*   Permission flags - see mkdir().
+	*	 Permission flags - see mkdir().
 	* @param int $options
-	*   A bit mask of STREAM_REPORT_ERRORS and STREAM_MKDIR_RECURSIVE.
+	*	 A bit mask of STREAM_REPORT_ERRORS and STREAM_MKDIR_RECURSIVE.
 	*
 	* @return bool
-	*   TRUE if directory was successfully created.
+	*	 TRUE if directory was successfully created.
 	*
 	* @see http://php.net/manual/streamwrapper.mkdir.php
 	*/
-	public function mkdir($uri, $mode, $options) {
-	$this->uri = $uri;
-	$recursive = (bool) ($options & STREAM_MKDIR_RECURSIVE);
-	if ($recursive) {
-	  // $this->getLocalPath() fails if $uri has multiple levels of directories
-	  // that do not yet exist.
-	  $localpath = $this->getDirectoryPath() . '/' . $this->getTarget($uri);
-	}
-	else {
-	  $localpath = $this->getLocalPath($uri);
-	}
-	if ($options & STREAM_REPORT_ERRORS) {
-	  return drupal_mkdir($localpath, $mode, $recursive);
-	}
-	else {
-	  return @drupal_mkdir($localpath, $mode, $recursive);
-	}
+	public function mkdir($uri, $mode, $options)
+	{
+		$this->uri = $uri;
+		$recursive = (bool) ($options & STREAM_MKDIR_RECURSIVE);
+		if ($recursive) {
+			// $this->getLocalPath() fails if $uri has multiple levels of directories
+			// that do not yet exist.
+			$localpath = $this->getDirectoryPath() . '/' . $this->getTarget($uri);
+		}
+		else {
+			$localpath = $this->getLocalPath($uri);
+		}
+		if ($options & STREAM_REPORT_ERRORS) {
+			return drupal_mkdir($localpath, $mode, $recursive);
+		}
+		else {
+			return @drupal_mkdir($localpath, $mode, $recursive);
+		}
 	}
 
 	/**
 	* Support for rmdir().
 	*
 	* @param string $uri
-	*   A string containing the URI to the directory to delete.
+	*	 A string containing the URI to the directory to delete.
 	* @param int $options
-	*   A bit mask of STREAM_REPORT_ERRORS.
+	*	 A bit mask of STREAM_REPORT_ERRORS.
 	*
 	* @return bool
-	*   TRUE if directory was successfully removed.
+	*	 TRUE if directory was successfully removed.
 	*
 	* @see http://php.net/manual/streamwrapper.rmdir.php
 	*/
-	public function rmdir($uri, $options) {
-	$this->uri = $uri;
-	if ($options & STREAM_REPORT_ERRORS) {
-	  return drupal_rmdir($this->getLocalPath());
-	}
-	else {
-	  return @drupal_rmdir($this->getLocalPath());
-	}
+	public function rmdir($uri, $options)
+	{
+		$this->uri = $uri;
+		if ($options & STREAM_REPORT_ERRORS) {
+			return drupal_rmdir($this->getLocalPath());
+		}
+		else {
+			return @drupal_rmdir($this->getLocalPath());
+		}
 	}
 
 	/**
 	* Support for stat().
 	*
 	* @param string $uri
-	*   A string containing the URI to get information about.
+	*	 A string containing the URI to get information about.
 	* @param int $flags
-	*   A bit mask of STREAM_URL_STAT_LINK and STREAM_URL_STAT_QUIET.
+	*	 A bit mask of STREAM_URL_STAT_LINK and STREAM_URL_STAT_QUIET.
 	*
 	* @return array
-	*   An array with file status, or FALSE in case of an error - see fstat()
-	*   for a description of this array.
+	*	 An array with file status, or FALSE in case of an error - see fstat()
+	*	 for a description of this array.
 	*
 	* @see http://php.net/manual/streamwrapper.url-stat.php
 	*/
-	public function url_stat($uri, $flags) {
-	$this->uri = $uri;
-	$path = $this->getLocalPath();
-	// Suppress warnings if requested or if the file or directory does not
-	// exist. This is consistent with PHP's plain filesystem stream wrapper.
-	if ($flags & STREAM_URL_STAT_QUIET || !file_exists($path)) {
-	  return @stat($path);
-	}
-	else {
-	  return stat($path);
-	}
+	public function url_stat($uri, $flags)
+	{
+		$this->uri = $uri;
+		$path = $this->getLocalPath();
+		// Suppress warnings if requested or if the file or directory does not
+		// exist. This is consistent with PHP's plain filesystem stream wrapper.
+		if ($flags & STREAM_URL_STAT_QUIET || !file_exists($path)) {
+			return @stat($path);
+		}
+		else {
+			return stat($path);
+		}
 	}
 
 	/**
 	* Support for opendir().
 	*
 	* @param string $uri
-	*   A string containing the URI to the directory to open.
+	*	 A string containing the URI to the directory to open.
 	* @param int $options
-	*   Unknown (parameter is not documented in PHP Manual).
+	*	 Unknown (parameter is not documented in PHP Manual).
 	*
 	* @return bool
-	*   TRUE on success.
+	*	 TRUE on success.
 	*
 	* @see http://php.net/manual/streamwrapper.dir-opendir.php
 	*/
-	public function dir_opendir($uri, $options) {
-	$this->uri = $uri;
-	$this->handle = opendir($this->getLocalPath());
+	public function dir_opendir($uri, $options)
+	{
+		$this->uri = $uri;
+		$this->handle = opendir($this->getLocalPath());
 
-	return (bool) $this->handle;
+		return (bool) $this->handle;
 	}
 
 	/**
 	* Support for readdir().
 	*
 	* @return string
-	*   The next filename, or FALSE if there are no more files in the directory.
+	*	 The next filename, or FALSE if there are no more files in the directory.
 	*
 	* @see http://php.net/manual/streamwrapper.dir-readdir.php
 	*/
-	public function dir_readdir() {
-	return readdir($this->handle);
+	public function dir_readdir()
+	{
+		return readdir($this->handle);
 	}
 
 	/**
 	* Support for rewinddir().
 	*
 	* @return bool
-	*   TRUE on success.
+	*	 TRUE on success.
 	*
 	* @see http://php.net/manual/streamwrapper.dir-rewinddir.php
 	*/
-	public function dir_rewinddir() {
-	rewinddir($this->handle);
-	// We do not really have a way to signal a failure as rewinddir() does not
-	// have a return value and there is no way to read a directory handler
-	// without advancing to the next file.
-	return TRUE;
+	public function dir_rewinddir()
+	{
+		rewinddir($this->handle);
+		// We do not really have a way to signal a failure as rewinddir() does not
+		// have a return value and there is no way to read a directory handler
+		// without advancing to the next file.
+		return TRUE;
 	}
 
 	/**
 	* Support for closedir().
 	*
 	* @return bool
-	*   TRUE on success.
+	*	 TRUE on success.
 	*
 	* @see http://php.net/manual/streamwrapper.dir-closedir.php
 	*/
-	public function dir_closedir() {
-	closedir($this->handle);
-	// We do not really have a way to signal a failure as closedir() does not
-	// have a return value.
-	return TRUE;
+	public function dir_closedir()
+	{
+		closedir($this->handle);
+		// We do not really have a way to signal a failure as closedir() does not
+		// have a return value.
+		return TRUE;
 	}
 }
