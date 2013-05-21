748c389be7fd264b4a31488e76899d0fdc15d87d
James Moss <james@message.co.uk>
2013-05-21 19:53:56 +0100

    Fixing parameter parsing

--- src/Message/Cog/DB/Query.php
+++ src/Message/Cog/DB/Query.php
@@ -16,7 +16,7 @@ class Query
 	protected $_query;
 	protected $_parsedQuery;
 
-	const TOKEN_REGEX = '/((\:[a-zA-Z0-9_\-\.]*)|\?)(([a-z]+))?/';
+	const TOKEN_REGEX = '/((\:[a-zA-Z0-9_\-\.]*)\??([a-z]*)?)|(\?([a-z]*))/us';
 
 	public function __construct(ConnectionInterface $connection)
 	{
@@ -71,6 +71,7 @@ class Query
 	{
 		if(!count($this->_params)) {
 			$this->_parsedQuery = $this->_query;
+
 			return false;
 		}
 
@@ -90,48 +91,53 @@ class Query
 			self::TOKEN_REGEX,
 			function($matches) use($fields, $types, &$counter, $connection) {
 
-			// parse and validate the token
-			$full  = $matches[0];
-			$param = isset($matches[3]) && !empty($matches[3]) ? $matches[3] : false;
-			$flags = isset($matches[6]) ? $matches[6] : 'sn';
-			$type  = str_replace('n', '', $flags, $useNull);
+				// parse and validate the token
+				$full  = $matches[0];
+				$param = substr($full, 0, 1) == ':' ? substr($matches[2], 1) : false; // The var after the colon.
+				$flagIndex = $param === false ? 5 : 3;
+				$flags = $matches[$flagIndex] ?: 'sn'; // data casting flags
+				$type  = str_replace('n', '', $flags, $useNull);
 
-			if(!isset($types[$type])) {
-				throw new Exception(sprintf('Unknown type `%s` in token `%s`', $type, $full));
-			}
+				if(!isset($types[$type])) {
+					throw new Exception(sprintf('Unknown type `%s` in token `%s`', $type, $full));
+				}
 
-			// decide what data to use
-			$data = null;
-			if($param !== false && isset($fields[$param])) {
-				$data = $fields[$param];
-			} else if($param === false  && $counter < count($fields)) {
-				$data = array_slice($fields, $counter, 1);
-				$data = reset($data);
-			}
-			$counter++;
+				// decide what data to use
+				$data = null;
+				if($param !== false && isset($fields[$param])) {
+					$data = $fields[$param];
+				} else if($param === false  && $counter < count($fields)) {
+					$data = array_slice($fields, $counter, 1);
+					$data = reset($data);
+				}
+				$counter++;
 
-			// check for nullness
-			if(is_null($data) && $useNull) {
-				return 'NULL';
-			}
+				// check for nullness
+				if(is_null($data) && $useNull) {
+					return 'NULL';
+				}
 
-			// santize
-			settype($data, $types[$type]);
-			$safe = $connection->escape($data);
+				// santize
+				settype($data, $types[$type]);
+				$safe = $connection->escape($data);
 
-			// format it ready for the query
-			if($type == 'd') {
-				if(ctype_digit($safe)) {
-					$safe = 'FROM_UNIXTIME('.$safe.')';
-				} else {
+				// format it ready for the query
+				if($type == 'd') {
+					if(ctype_digit($safe)) {
+						$safe = 'FROM_UNIXTIME('.$safe.')';
+					} else {
+						$safe = "'".$safe."'";
+					}
+				} elseif($type == 's' || $type == 'f') {
 					$safe = "'".$safe."'";
 				}
-			} elseif($type == 's' || $type == 'f') {
-				$safe = "'".$safe."'";
-			}
 
-			return $safe;
-		}, $this->_query);
+				return $safe;
+			}, 
+		$this->_query);
+
+echo(	$this->_parsedQuery);
+
 
 		return true;
 	}
