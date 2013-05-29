fbca0d1f365fc60f7980655f10a6972c747f6bb8
James Moss <james@message.co.uk>
2013-05-29 16:59:53 +0100

    Better docs for Finder

--- src/Message/Cog/Filesystem/Finder.php
+++ src/Message/Cog/Filesystem/Finder.php
@@ -6,7 +6,8 @@ use Symfony\Component\Finder\Finder as SymfonyFinder;
 
 /**
 * A wrapper for Cog around Symfony's Finder component
-* http://symfony.com/doc/master/components/finder.html
+* 
+* @see  http://symfony.com/doc/master/components/finder.html
 *
 * @author  James Moss <james@message.co.uk>
 */
@@ -15,6 +16,10 @@ class Finder extends SymfonyFinder
 	/**
 	 * Before the finder returns the iterator, inject our own FileIterator which ensures
 	 * a File object is always returned.
+	 * 
+	 * Iterators stack up, so this code iterates the $parent iterator - but we want this. 
+	 * Our FileIterator is the last in the chain so when it iterates over the
+	 * elements from $parent it returns them all as a Cog\File object.
 	 *
 	 * @return FileIterator The iterator which injects a File object
 	 */
