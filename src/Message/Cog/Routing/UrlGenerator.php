6b018936965a672c509aa08130d7b8432a6edd1c
Joe Holdcroft <joe@message.co.uk>
2013-06-05 13:15:25 +0100

    Getting rid of RouterInterface

--- src/Message/Cog/Routing/UrlGenerator.php
+++ src/Message/Cog/Routing/UrlGenerator.php
@@ -2,6 +2,10 @@
 
 namespace Message\Cog\Routing;
 
+/**
+ * Empty wrapper around Symfony's `UrlGenerator` class so we're not exposing
+ * any Symfony code to the rest of Cog.
+ */
 class UrlGenerator extends \Symfony\Component\Routing\Generator\UrlGenerator
 {
 }
