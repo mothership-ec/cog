8d54551c2d3444c9a0bd74937e1d7265f2c56322
James Moss <james@message.co.uk>
2013-06-07 17:34:14 +0100

    Removing debugging code

--- src/Message/Cog/ImageResize/Controller.php
+++ src/Message/Cog/ImageResize/Controller.php
@@ -6,16 +6,12 @@ class Controller extends \Message\Cog\Controller\Controller
 {
 	public function index($url)
 	{
-		file_put_contents('cog://tmp/mossdebug.log', print_r(array($url), true), FILE_APPEND);
-
 		try {
 			$resizer = $this->get('image.resize');
 			$saved = $resizer->resize($url);	
 		} catch(\Exception $e) {
 			throw $this->createNotFoundException($e->getMessage());
 		}
-
-		file_put_contents('cog://tmp/mossdebug.log', print_r(array($url, $saved), true), FILE_APPEND);
 
 		return new \Symfony\Component\HttpFoundation\BinaryFileResponse($saved->realpath());
 	}
