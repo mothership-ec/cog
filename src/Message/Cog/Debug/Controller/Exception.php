9e129b1600a01d0ec0d434b58c130eb1b12bc214
Joe Holdcroft <joe@message.co.uk>
2013-06-15 19:19:48 +0100

    Adding code lines to exception page #35

--- src/Message/Cog/Debug/Controller/Exception.php
+++ src/Message/Cog/Debug/Controller/Exception.php
@@ -4,12 +4,39 @@ namespace Message\Cog\Debug\Controller;
 
 use Message\Cog\Controller\Controller;
 
+use Message\Cog\Filesystem\File;
+
 class Exception extends Controller
 {
 	public function viewException($exception)
 	{
 		return $this->render('Message:Cog:Debug::exception', array(
 			'exception' => $exception,
+		));
+	}
+
+	public function filePeek($file, $line, $offset = 2)
+	{
+		$file  = new File($file);
+		$lines = array();
+
+		if ($file->isReadable()) {
+			$file = $file->openFile();
+			$file->seek($line - $offset);
+
+			// Start a loop for the number of lines to fetch
+			for ($i = 0; $i <= (($offset * 2) + 1); $i++) {
+				// Break the loop if we're at EOF
+				if (!$file->valid()) {
+					break;
+				}
+				$lines[$file->key()] = $file->current();
+				$file->next();
+			}
+		}
+
+		return $this->render('Message:Cog:Debug::file_peek', array(
+			'lines' => $lines,
 		));
 	}
 }
