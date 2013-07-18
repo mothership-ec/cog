be0ad59e52293af81cd478f31218db2c89cf1e1a
Chad Tomkiss <chadtomkiss@hotmail.co.uk>
2013-07-18 11:50:31 +0100

    Fix symlink issue by specifying a relative path

--- src/Message/Cog/Console/Command/AssetDump.php
+++ src/Message/Cog/Console/Command/AssetDump.php
@@ -1,84 +1,91 @@
 <?php
 
-	namespace Message\Cog\Console\Command;
+namespace Message\Cog\Console\Command;
 
-	use Message\Cog\Console\Command;
-	use Message\Cog\Filesystem\File;
+use Message\Cog\Console\Command;
+use Message\Cog\Filesystem\File;
 
-	use Symfony\Component\Console\Input\InputArgument;
-	use Symfony\Component\Console\Input\InputInterface;
-	use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-	/**
-	 * AssetDump
-	 *
-	 * Provides the asset:dump command.
-	 * Move module assets to public folder.
-	 */
-	class AssetDump extends Command
+/**
+ * AssetDump
+ *
+ * Provides the asset:dump command.
+ * Move module assets to public folder.
+ */
+class AssetDump extends Command
+{
+	const USE_SYMLINKS = 'symlink';
+
+	protected function configure()
 	{
-		const USE_SYMLINKS = 'symlink';
+		$this
+			->setName('asset:dump')
+			->setDescription('Move module assets to public folder.')
+		;
 
-		protected function configure()
-		{
-			$this
-				->setName('asset:dump')
-				->setDescription('Move module assets to public folder.')
-			;
+		// Whether to use symlinks to copy assets across
+		$this->addOption('--' . self::USE_SYMLINKS);
+	}
 
-			// Whether to use symlinks to copy assets across
-			$this->addOption('--' . self::USE_SYMLINKS);
-		}
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		// Service to work with files
+		$fileSystem = $this->get('filesystem');
 
-		protected function execute(InputInterface $input, OutputInterface $output)
-		{
-			// Service to work with files
-			$fileSystem = $this->get('filesystem');
+		// Base Location for Public Assets
+		$basePath = 'cog://';
+		$cogulePath = 'public/cogules/';
 
-			// Base Location for Public Assets
-			$filePath = 'cog://public/cogules/';
-			$resourcesDir = 'resources/public/';
+		$filePath = $basePath . $cogulePath;
+		$resourcesDir = 'resources/public/';
 
-			// Get List of Loaded Modules
-			$modules = $this->get('module.loader')->getModules();
+		// Get List of Loaded Modules
+		$modules = $this->get('module.loader')->getModules();
 
-			// Service to locate module paths
-			$moduleLocator = $this->get('module.locator');
+		// Service to locate module paths
+		$moduleLocator = $this->get('module.locator');
 
-			$output->writeln('<info>Moving public assets for ' . count($modules) . ' modules.</info>');
+		// Determine whether to use symlinks to map resources.
+		$useSymlinks = $input->getOption('symlink');
 
-			$useSymlinks = $input->getOption('symlink');
+		$output->writeln('<info>Moving public assets for ' . count($modules) . ' modules.</info>');
 
-			foreach($modules as $module) {
-				$moduleName = str_replace("\\", ':', $module);
+		foreach($modules as $module) {
+			$moduleName = str_replace("\\", ':', $module);
 
-				// Directory locations
-				$originDir = $moduleLocator->getPath($module) . $resourcesDir;
-				$targetDir = $filePath . $moduleName;
+			// Directory locations
+			$originDir = $moduleLocator->getPath($module) . $resourcesDir;
+			$targetDir = $filePath . $moduleName;
 
-				// Move on to next module if there are no public resources.
-				if(!$fileSystem->exists($originDir))
-				{
-					$output->writeln("No resources for {$module}");
-					continue;
-				}
+			// Move on to next module if there are no public resources.
+			if(!$fileSystem->exists($originDir))
+			{
+				$output->writeln("<info>No resources for {$module}</info>");
+				continue;
+			}
 
-				$output->writeln("Copying {$originDir} to {$targetDir}");
-				$output->writeln('');
+			$output->writeln("Copying {$originDir} to {$targetDir}");
+			$output->writeln('');
 
-				/*
-				 * Copy files across to public folder
-				 *
-				 * Either create a symlink, or copy files across.
-				 */
-				if($useSymlinks)
-				{
-					$fileSystem->symlink($originDir, $targetDir);
-				}
-				else
-				{
-					$fileSystem->mirror($originDir, $targetDir);
-				}
+			/*
+			 * Copy files across to public folder
+			 *
+			 * Either create a symlink, or copy files across.
+			 */
+			if($useSymlinks)
+			{
+				// Symlink needs to be a relative path to project root
+				$symlinkDir = $cogulePath . $moduleName;
+
+				$fileSystem->symlink($originDir, $symlinkDir);
+			}
+			else
+			{
+				$fileSystem->mirror($originDir, $targetDir);
 			}
 		}
 	}
+}
