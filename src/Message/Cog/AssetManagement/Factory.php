00717b6e3bd015dcafd64b1c1e448abced710f59
Joe Holdcroft <joe@message.co.uk>
2013-08-07 15:57:36 +0100

    Working on asset relocation stuff

--- src/Message/Cog/AssetManagement/Factory.php
+++ src/Message/Cog/AssetManagement/Factory.php
@@ -21,6 +21,7 @@ class Factory extends AssetFactory
 
         foreach ($inputs as $key => $input) {
         	$inputs[$key] = $this->_referenceParser->parse($input)->getFullPath('View');
+        	$inputs[$key] = $this->_referenceParser->parse($input)->getFullPath();
         }
 
         return parent::createAsset($inputs, $filters, $options);
