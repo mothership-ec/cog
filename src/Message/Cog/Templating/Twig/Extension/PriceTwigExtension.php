40ca7d6c48a67c0b3b86d9561bc715bdefad0bdc
Iris Schaffer <irisSchaffer@gmx.net>
2013-08-08 11:36:13 +0100

    Changed price-filter to use default locale + default currency for that locale if no currency-code is given

--- src/Message/Cog/Templating/Twig/Extension/PriceTwigExtension.php
+++ src/Message/Cog/Templating/Twig/Extension/PriceTwigExtension.php
@@ -11,11 +11,15 @@ class PriceTwigExtension extends \Twig_Extension
         );
     }
 
-    public function priceFilter($value, $currency = 'GBP', $locale = null)
+    public function priceFilter($value, $currency = null, $locale = null)
     {
- 		$locale = ($locale == null ? \Locale::getDefault() : $locale);
- 		$formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        // if no locale was given, use default one
+ 		$locale = (is_null($locale) ? \Locale::getDefault() : $locale);
 
+        $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+
+        // if no currency-code was given, get currency code from locale
+        $currency = (is_null($currency) ? $formatter->getTextAttribute(\NumberFormatter::CURRENCY_CODE) : $currency);
         $price = $formatter->formatCurrency($value, $currency);
 
         return $price;
