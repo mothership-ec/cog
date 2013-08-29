2c36285fcd72505e56097a57b841219d819e8fc5
Laurence Roberts <lsjroberts@gmail.com>
2013-08-29 14:49:44 +0100

    Refactored ViewNameParser to allow an 'all' method

--- src/Message/Cog/Templating/ViewNameParser.php
+++ src/Message/Cog/Templating/ViewNameParser.php
@@ -4,6 +4,7 @@ namespace Message\Cog\Templating;
 
 use Message\Cog\Service\ContainerInterface;
 use Message\Cog\Module\ReferenceParserInterface;
+use Message\Cog\Filesystem\Finder;
 
 use Symfony\Component\Templating\TemplateReference;
 use Symfony\Component\Templating\TemplateNameParser;
@@ -27,12 +28,13 @@ class ViewNameParser extends TemplateNameParser
 	 * @param array                    $fileTypes Array of filetypes to support, in order of preference
 	 * @param array                    $fileTypes Array of formats to support, in order of preference
 	 */
-	public function __construct(ContainerInterface $services, ReferenceParserInterface $parser, array $fileTypes, array $formats)
+	public function __construct(ContainerInterface $services, ReferenceParserInterface $parser, Finder $finder, array $fileTypes, array $formats)
 	{
 		$this->_services  = $services;
 		$this->_parser    = $parser;
+		$this->_finder    = $finder;
 		$this->_fileTypes = $fileTypes;
-		$this->_formats = $formats;
+		$this->_formats   = $formats;
 	}
 
 	/**
@@ -54,10 +56,88 @@ class ViewNameParser extends TemplateNameParser
 	 */
 	public function parse($reference)
 	{
+		$baseFileName = $this->getFileName($reference);
+
+		if (false === $template = $this->getTemplateReference($baseFileName)) {
+			throw new NotAcceptableHttpException(sprintf(
+				'View format could not be determined for reference `%s`',
+				$reference
+			));
+		}
+
+		return $template;
+	}
+
+	/**
+	 * Parse a view reference and find all views that match regardless of
+	 * format.
+	 * 
+	 * @param  string $reference The view reference (without the format)
+	 * @return Finder            A finder with the files
+	 */
+	public function all($reference)
+	{
+		$baseFileName = $this->getFileName($reference);
+
+		$files = $this->_finder->files()->in(dirname($baseFileName));
+
+		return $files;
+	}
+
+	/**
+	 * Get the file name matching the reference.
+	 * 
+	 * @param  string $reference The view reference (without the format)
+	 * @return string            The base file name
+	 */
+	public function getFileName($reference)
+	{
 		// Get the current HTTP request
 		$request = $this->_services['request'];
 		$parsed  = $this->_parser->parse($reference);
 
+		$parsed = $this->getAbsolute($parsed);
+
+		// Force the parser to not look in the library
+		$parsed->setInLibrary(false);
+
+		// Get the base file name from the reference parser
+		$baseFileName = $parsed->getFullPath('resources/view');
+
+		return $baseFileName;
+	}
+
+	/**
+	 * Get the template reference for the file name that first matches theallowed formats & engine.
+	 * 
+	 * @param  string            $baseFileName The base file name
+	 * @return TemplateReference
+	 */
+	public function getTemplateReference($baseFileName)
+	{
+		// Loop through each content type
+		foreach ($this->_formats as $format) {
+			// Loop through the engines in order of preference
+			foreach ($this->_fileTypes as $engine) {
+				// Check if a view file exists for this format and this engine
+				$fileName = $baseFileName . '.' . $format . '.' . $engine;
+				if (file_exists($fileName)) {
+					return new TemplateReference($fileName, $engine);
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get the absolute path for a parsed reference.
+	 * 
+	 * @param  ReferenceParser $parsed Parsed reference
+	 * @return ReferenceParser         Absolute parsed reference
+	 */
+	public function getAbsolute($parsed)
+	{
 		// If it is relative and an absolute path was used previously, make the
 		// reference absolute using the previous module name
 		// This is a fix for https://github.com/messagedigital/cog/issues/40
@@ -74,27 +154,8 @@ class ViewNameParser extends TemplateNameParser
 			$this->_lastAbsoluteModule = $parsed->getModuleName();
 		}
 
-		// Force the parser to not look in the library
-		$parsed->setInLibrary(false);
-
-		// Get the base file name from the reference parser
-		$baseFileName = $parsed->getFullPath('resources/view');
-
-		// Loop through each content type
-		foreach ($this->_formats as $format) {
-			// Loop through the engines in order of preference
-			foreach ($this->_fileTypes as $engine) {
-				// Check if a view file exists for this format and this engine
-				$fileName = $baseFileName . '.' . $format . '.' . $engine;
-				if (file_exists($fileName)) {
-					return new TemplateReference($fileName, $engine);
-				}
-			}
-		}
-
-		throw new NotAcceptableHttpException(sprintf(
-			'View format could not be determined for reference `%s`',
-			$reference
-		));
+		return $parsed;
 	}
+
+
 }
