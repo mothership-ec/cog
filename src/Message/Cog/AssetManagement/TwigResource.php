26d870e1089d11311bd00e30f30987b4fca14b22
Laurence Roberts <lsjroberts@gmail.com>
2013-10-21 14:58:00 +0100

    Extended TwigResource to ensure __toString is correctly casted

---
+++ src/Message/Cog/AssetManagement/TwigResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Message\Cog\AssetManagement;
+
+use Assetic\Extension\Twig\TwigResource as AsseticTwigResource;
+
+class TwigResource extends AsseticTwigResource {
+
+	public function __toString()
+	{
+		// Ensure that the name property is cast as a string before returning.
+		// This fixes the issue of PHP not bubbling down the __toString()
+		// methods.
+		// @see https://gist.github.com/lsjroberts/7084126
+		return (string) $this->name;
+	}
+
+}
