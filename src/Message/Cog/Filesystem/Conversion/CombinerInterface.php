932dc251b6966448e0edb4cf0f5175e049b1ad1e
Laurence Roberts <lsjroberts@gmail.com>
2013-10-24 14:08:49 +0100

    Added combine method to pdf converter

--- src/Message/Cog/Filesystem/Conversion/CombinerInterface.php
+++ src/Message/Cog/Filesystem/Conversion/CombinerInterface.php
@@ -2,14 +2,26 @@
 
 namespace Message\Cog\Filesystem\Conversion;
 
+/**
+ * Combines multiple files into one.
+ *
+ * @usage
+ *     $converter = $this->get('filesystem.conversion.pdf');
+ *     $file = $converter->combine('combined.pdf', array(
+ *         $converter->setView('::view-1')->save('view-1.pdf'),
+ *         $converter->setView('::view-2')->save('view-2.pdf'),
+ *         $converter->setView('::view-3')->save('view-3.pdf'),
+ *     ));
+ */
 interface CombinerInterface {
 
 	/**
 	 * Combines multiple documents into one document.
 	 *
-	 * @param  ... \Message\Cog\Filesystem\File
+	 * @param  string    $path Path to save the combined file.
+	 * @param  ...[File] $files
 	 * @return \Message\Cog\Filesystem\File The combined file.
 	 */
-	public function combine();
+	public function combine($path, array $files = null);
 
 }
--- src/Message/Cog/Filesystem/Conversion/PDFConverter.php
+++ src/Message/Cog/Filesystem/Conversion/PDFConverter.php
@@ -4,10 +4,12 @@ namespace Message\Cog\Filesystem\Conversion;
 
 use InvalidArgumentException;
 
+use fpdf\FPDF;
+use fpdi\FPDI;
 use Knp\Snappy\Pdf;
 use Message\Cog\Filesystem\File;
 
-class PDFConverter extends AbstractConverter {
+class PDFConverter extends AbstractConverter implements CombinerInterface {
 
 	/**
 	 * {@inheritDoc}
@@ -33,6 +35,45 @@ class PDFConverter extends AbstractConverter {
 		$pdf->generateFromHTML($html, $path);
 
 		return new File($path);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function combine($path, array $files = null)
+	{
+		$files = is_array($files) ? $files : array_slice(func_get_args(), 1);
+
+		if (! $files or 0 === count($files)) {
+			return false;
+		}
+
+		if (1 === count($files)) {
+			return $files[0];
+		}
+
+		$combined = new FPDI;
+
+		foreach ($files as $file) {
+			if (! $file instanceof File) {
+				$file = new File($file);
+			}
+
+			// Get each page of each file
+			$pageCount = $combined->setSourceFile($file->getRealPath());
+			for ($i = 1; $i <= $pageCount; $i++) {
+				$page = $combined->importPage($i);
+				$size = $combined->getTemplateSize($page);
+				$combined->AddPage($size['w'] > $size['h'] ? 'L' : 'P', array($size['w'], $size['h']));
+				$combined->useTemplate($page);
+			}
+		}
+
+		$dest = new File($path);
+
+		$combined->Output($dest->getRealPath(), 'F');
+
+		return $dest;
 	}
 
 }
