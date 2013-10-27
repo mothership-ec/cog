213a881e6cb666249be8160d4720f03b1c153e11
Iris Schaffer <irisSchaffer@gmx.net>
2013-10-27 16:25:54 +0100

    Validator: Added getField($name) method

--- src/Message/Cog/Validation/Validator.php
+++ src/Message/Cog/Validation/Validator.php
@@ -81,6 +81,23 @@ class Validator
 	}
 
 	/**
+	 * Gets field by given name or field current field
+	 * @param  string $name name of field, defaults to null
+	 * 
+	 * @throws \Exception   If no field with name $name exists
+	 * 
+	 * @return Field        Field with name $name or $_fieldPointer
+	 */
+	public function getField($name = null)
+	{
+		if($name && !array_key_exists($name, $this->_fields)) {
+			throw new \Exception(sprintf('Form field `%s` does not exist.', $name));
+		}
+
+		return $name ? $this->_fields[$name] : $this->_fieldPointer;
+	}
+
+	/**
 	 * Add a new field to be validated and adjusts the fieldPointer
 	 *
 	 * @param  Field $field         the field to be added
