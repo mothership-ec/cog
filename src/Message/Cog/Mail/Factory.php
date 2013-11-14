6dcc6c25ae8c6c8886ee8d3ec061ad7bc2353679
Laurence Roberts <lsjroberts@gmail.com>
2013-11-14 09:54:30 +0000

    Added abstract mail factory

---
+++ src/Message/Cog/Mail/Factory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Message\Cog\Mail;
+
+use Message\Cog\Service\ContainerInterface;
+use Message\Cog\Service\ContainerAwareInterface;
+
+abstract class Factory implements ContainerAwareInterface {
+
+	protected $_container;
+	protected $_built = false;
+
+	public function __construct(ContainerInterface $container)
+	{
+		$this->setContainer($container);
+		$this->_message = $this->get('mail.message');
+	}
+
+	public function setContainer(ContainerInterface $container)
+	{
+		$this->_container = $container;
+	}
+
+	public function get($service)
+	{
+		return $this->_container[$service];
+	}
+
+	abstract public function build($params = array())
+	{
+		$this->_built = true;
+	}
+
+	public function send()
+	{
+		try {
+			if (!$this->_built) {
+				throw new \LogicException('Message must be built first!');
+			}
+
+			return $this->get('mail.dispatcher')->send($this->_message);
+		}
+		catch (\Exception $e) {
+			$this->get('log.errors')->error('Message:Cog:Mail:Factory', array(
+				'exception' => $e,
+			));
+
+			return false;
+		}
+	}
+
+}
