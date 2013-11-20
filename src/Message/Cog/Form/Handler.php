a32e4d692df0980d93ae306b143c245b018f16b6
Laurence Roberts <lsjroberts@gmail.com>
2013-11-20 13:57:58 +0000

    Added function to attempt detection of the date format prior to instantiating as DateTime

--- src/Message/Cog/Form/Handler.php
+++ src/Message/Cog/Form/Handler.php
@@ -511,9 +511,35 @@ class Handler
 		// Sometimes date fields do not automatically get formatted to a
 		// Datetime object due to browser differences. This ensures all dates
 		// are instantiated correctly.
+		$detectDateFormat = function($date) {
+			// Get the split character
+			if (false !== strstr($date, '/')) $split = '/';
+			if (false !== strstr($date, '-')) $split = '-';
+
+			// Check if the date starts with dd or mm
+			if (preg_match('/[0-9]{2}(\/|-)[0-9]{2}(\/|-)[0-9]{2,4}/', $date)) {
+				$parts = explode($split, $date);
+
+				// Do a dumb check to see if the second value is greater than 12
+				// then it must be a dd instead of mm, and thus mm/dd/yyyy
+				if ($parts[1] > 12) {
+					return 'm'.$split.'d'.$split.'Y';
+				}
+
+				return 'd'.$split.'m'.$split.'Y';
+			}
+			// Else check if the date starts with yyyy
+			elseif (preg_match('/[0-9]{2,4}(\/|-)[0-9]{2}(\/|-)[0-9]{2}/', $date)) {
+				return 'Y'.$split.'m'.$split.'d';
+			}
+
+			throw new \Exception(sprintf("Could not determine date format of '%s'", $date));
+		};
+
 		foreach ($this->_fields as $name => $field) {
 			if ('date' == $field['type'] and $value = $data[$name] and ! $value instanceof \DateTime) {
-				$data[$name] = \DateTime::createFromFormat('Y-m-d', $value);
+				$format = $detectDateFormat($value);
+				$data[$name] = \DateTime::createFromFormat($format, $value);
 			}
 		}
 
