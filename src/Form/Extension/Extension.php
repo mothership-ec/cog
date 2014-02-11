e3cbde322a5e2d5043377ac11926071df848a815
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-02-11 15:25:34 +0000

    moved LinkedChoice form type to cog

--- src/Form/Extension/Extension.php
+++ src/Form/Extension/Extension.php
@@ -14,6 +14,7 @@ class Extension extends AbstractExtension
 			new Type\DateType(),
 			new Type\TimeType(),
 			new Type\DatalistType(),
+//			new Type\LinkedChoice(),  @todo needs an array as its param, maybe look into refactoring this thing
 		);
 	}
 }
