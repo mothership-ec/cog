23bbb9b6ca5ff6f98d0a1b8405442626a4b980f8
Joe Holdcroft <joe@message.co.uk>
2014-03-27 16:16:27 +0000

    Add Asset class for an Asset defined by a Cog reference

---
+++ src/AssetManagement/FileReferenceAsset.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Message\Cog\AssetManagement;
+
+use Message\Cog\Module\ReferenceParserInterface;
+
+use Assetic\Asset\FileAsset;
+
+/**
+ * Asset for a file using a Cog reference. The reference is parsed before it is
+ * passed on to the base FileAsset class.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class FileReferenceAsset extends FileAsset
+{
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param ReferenceParserInterface $parser The reference parser
+	 * @param string                   $source     An absolute path
+	 * @param array                    $filters    An array of filters
+	 * @param string                   $sourceRoot The source asset root directory
+	 * @param string                   $sourcePath The source asset path
+	 * @param array                    $vars
+	 *
+	 * @throws \InvalidArgumentException If the supplied root doesn't match the
+	 *                                   source when guessing the path
+	 */
+	public function __construct(ReferenceParserInterface $parser, $source, $filters = array(), $sourceRoot = null, $sourcePath = null, array $vars = array())
+	{
+		$source = $parser->parse($source)->getFullPath();
+
+		return parent::__construct($source, $filters, $sourceRoot, $sourcePath, $vars);
+	}
+}
