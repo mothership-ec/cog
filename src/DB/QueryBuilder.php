5af13c58227f0b980dea418099506a5ac18971e6
Eleanor Shakeshaft <eleanor@message.co.uk>
2014-09-12 17:05:26 +0100

    Adding methods to QueryBuilder and updating QueryBuilderInterface

--- src/DB/QueryBuilder.php
+++ src/DB/QueryBuilder.php
@@ -2,7 +2,235 @@
 
 namespace Message\Cog\DB;
 
-class QueryBuilder
+class QueryBuilder implements QueryBuilder
 {
-	
+	const SELECT    = 'SELECT';
+	const FROM      = 'FROM';
+	const JOIN      = 'JOIN';
+	const LEFT_JOIN = 'LEFT JOIN';
+	const UNION     = 'UNION';
+	const UNION_ALL = 'UNION ALL';
+	const WHERE     = 'WHERE';
+	const GROUP_BY  = 'GROUP BY';
+	const ORDER_BY  = 'ORDER BY';
+	const ORDER_BY  = 'ORDER BY';
+	const HAVING    = 'HAVING';
+	const LIMIT     = 'LIMIT';
+
+	private $_start = [];
+	private $_from = [];
+	private $_join = [];
+	private $_leftJoin = [];
+	private $_union = [];
+	private $_unionAll = [];
+	private $_where = [];
+	private $_groupBy = [];
+	private $_orderBy = [];
+	private $_having = [];
+	private $_limit;
+
+
+
+	/**
+	 * Adds items used to build up select statement
+	 *
+	 * @param  string | array        $select Item(s) to add to select statement
+	 *
+	 * @return QueryBuilder                  $this
+	 */
+	public function select($select)
+	{
+		$this->_start[] = $select;
+
+		return $this;
+	}
+
+	/**
+	 * Table name and optional query to select from
+	 *
+	 * @param  string                $tableName  Name of the table
+	 * @param  QueryBuilderInterface $builder    QueryBuilder to provide data
+	 *
+	 * @return QueryBuilder                      $this
+	 */
+	public function from($tableName, QueryBuilderInterface $builder = null)
+	{
+		$this->_from[$tableName] = $builder ?: $tableName;
+
+		return $this;
+	}
+
+	/**
+	 * Joins the current query to table $tableName or data specified by
+	 * $builder as $tableName using $onStatement.
+	 *
+	 * @param  string                $tableName   name of the table
+	 * @param  string                $onStatement statement to join on
+	 * @param  QueryBuilderInterface $builder     optional way
+	 *
+	 * @return QueryBuilder                       $this
+	 */
+	public function join($tableName, $onStatement, QueryBuilderInterface $builder = null)
+	{
+		$this->_join[$tableName] = [
+			['on'    => $onStatement],
+			['table' => $builder ?: $tableName ]
+		]
+
+		return $this;
+	}
+
+	/**
+	 * Joins the current query to table $tableName or data specified by
+	 * $builder as $tableName using $onStatement.
+	 *
+	 * @param  string                $tableName   name of the table
+	 * @param  string                $onStatement statement to join on
+	 * @param  QueryBuilderInterface $builder     optional way
+	 *
+	 * @return QueryBuilder                       $this
+	 */
+	public function leftJoin($tableName, $onStatement, QueryBuilderInterface $builder = null)
+	{
+		$this->_leftJoin[$tableName] = [
+			['on'    => $onStatement],
+			['table' => $builder ?: $tableName ]
+		]
+
+		return $this;
+	}
+
+	/**
+	 * [union description]
+	 * @return [type] [description]
+	 */
+	public function union()
+	{
+		if ($this->_validate($item)) {
+			$unions = FUNC_GET_ARGS();
+
+		}
+
+	}
+
+	/**
+	 * [unionAll description]
+	 * @return [type] [description]
+	 */
+	public function unionAll()
+	{
+
+	}
+
+	/**
+	 * [where description]
+	 * @param  [type]  $statement [description]
+	 * @param  [type]  $variable  [description]
+	 * @param  boolean $and       [description]
+	 * @return [type]             [description]
+	 */
+	public function where($statement, $variable = null, $and = true)
+	{
+
+	}
+
+	/**
+	 * Groups by fields in $groupBy
+	 *
+	 * @param  string | array        $groupBy Fields to group by
+	 *
+	 * @return QueryBuilderInterface           $this
+	 */
+	public function groupBy($groupBy)
+	{
+		$this->_groupBy[] = $groupBy;
+
+		return $this;
+	}
+
+	/**
+	 * Orders by fields in $orderBy
+	 *
+	 * @param  string | array        $orderBy Fields to order by
+	 *
+	 * @return QueryBuilderInterface           $this
+	 */
+	public function orderBy($orderBy);
+	{
+		$this->_orderBy[] = $orderBy;
+
+		return $this;
+	}
+
+
+	/**
+	 * [having description]
+	 * @param  [type]  $statement [description]
+	 * @param  [type]  $variable  [description]
+	 * @param  boolean $and       [description]
+	 * @return [type]             [description]
+	 */
+	public function having($statement, $variable = null, $and = true)
+	{
+
+	}
+
+	/**
+	 * Limits query to $limit rows
+	 *
+	 * @param  int $limit the limit
+	 *
+	 * @return QueryBuilderInterface $this
+	 */
+	public function limit($limit)
+	{
+		$this->_limit = $limit;
+
+		return $this;
+	}
+
+
+	/**
+	 * [clear description]
+	 * @return [type] [description]
+	 */
+	public function clear()
+	{
+
+	}
+
+	/**
+	 * [getQuery description]
+	 * @return [type] [description]
+	 */
+	public function getQuery()
+	{
+
+	}
+
+	/**
+	 * [getQueryString description]
+	 * @return [type] [description]
+	 */
+	public function getQueryString()
+	{
+
+	}
+
+
+	/**
+	 * Checks if item given is an instance of QueryBuilder.
+	 *
+	 * @param  mixed $item The item to validate
+	 *
+	 * @return bool False if it's not of type QueryBuilder
+	 */
+	private function _validateQueryBuilder($item)
+	{
+		if ($item instanceof QueryBuilder) {
+			return true;
+		}
+
+		throw new \InvalidArgumentException('Is not an instance of QueryBuilder');
+	}
 }
--- src/DB/QueryBuilderInterface.php
+++ src/DB/QueryBuilderInterface.php
@@ -6,107 +6,129 @@ interface QueryBuilderInterface
 {
 	/**
 	 * Adds items used to build up select statement
+	 *
 	 * @param  string | array        $select Item(s) to add to select statement
+	 *
 	 * @return QueryBuilderInterface         $this
 	 */
 	public function select($select);
 
 	/**
-	 * Table name and optional query to select from, calling multiple 
-	 * times will build up with union
+	 * Table name and optional query to select from
+	 *
 	 * @param  string                $tableName  Name of the table
 	 * @param  QueryBuilderInterface $builder    QueryBuilder to provide data
+	 *
 	 * @return QueryBuilderInterface             $this
 	 */
 	public function from($tableName, QueryBuilderInterface $builder = null);
 
 	/**
-	 * Builds UNION block takes [$var1, [$var2, [$var3...]...]...] each being 
-	 * string or QueryBuilder
-	 * @return QueryBuilderInterface $this
-	 */
-	public function union();
-
-	/**
-	 * Builds UNION block takes [$var1, [$var2, [$var3...]...]...] each being 
-	 * string or QueryBuilder
-	 * @return QueryBuilderInterface $this
-	 */
-	public function unionAll();
-
-	/**
 	 * Joins the current query to table $tableName or data specified by
-	 * $builder as $tableName on $onStatement.
+	 * $builder as $tableName using $onStatement.
+	 *
 	 * @param  string                $tableName   name of the table
 	 * @param  string                $onStatement statement to join on
-	 * @param  QueryBuilderInterface $builder     optional way 
-	 * @return QueryBuilderInterface              $this            
+	 * @param  QueryBuilderInterface $builder     optional way
+	 *
+	 * @return QueryBuilderInterface              $this
 	 */
 	public function join($tableName, $onStatement, QueryBuilderInterface $builder = null);
 
 	/**
 	 * Joins the current query to table $tableName or data specified by
-	 * $builder as $tableName on $onStatement.
+	 * $builder as $tableName using $onStatement.
+	 *
 	 * @param  string                $tableName   name of the table
 	 * @param  string                $onStatement statement to join on
-	 * @param  QueryBuilderInterface $builder     optional way 
-	 * @return QueryBuilderInterface              $this            
+	 * @param  QueryBuilderInterface $builder     optional way
+	 *
+	 * @return QueryBuilderInterface              $this
 	 */
 	public function leftJoin($tableName, $onStatement, QueryBuilderInterface $builder = null);
 
 	/**
-	 * Limits query to $limit rows
-	 * @param  int $limit the limit
+	 * Builds UNION block takes [$var1, [$var2, [$var3...]...]...] each being
+	 * string or QueryBuilder
+	 *
 	 * @return QueryBuilderInterface $this
 	 */
-	public function limit($limit);
+	public function union();
+
+	/**
+	 * Builds UNION block takes [$var1, [$var2, [$var3...]...]...] each being
+	 * string or QueryBuilder
+	 *
+	 * @return QueryBuilderInterface $this
+	 */
+	public function unionAll();
+
+	/**
+	 * Builds into the where statement using "AND" as default.
+	 *
+	 * @param  string | closure  $statement the statement to append
+	 * @param  mixed             $variable  variable to substitute in
+	 * @param  boolean           $and       append using and if true, or if false
+	 *
+	 * @return QueryBuilderInterface         $this
+	 */
+	public function where($statement, $variable = null, $and = true);
 
 	/**
 	 * Groups by fields in $groupBy
+	 *
 	 * @param  string | array        $groupBy Fields to group by
+	 *
 	 * @return QueryBuilderInterface           $this
 	 */
 	public function groupBy($groupBy);
 
 	/**
 	 * Orders by fields in $orderBy
+	 *
 	 * @param  string | array        $orderBy Fields to order by
+	 *
 	 * @return QueryBuilderInterface           $this
 	 */
 	public function orderBy($orderBy);
 
 	/**
-	 * Builds into the where statement using and as default.
-	 * @param  string | closure  $statement the statement to append
-	 * @param  mixed             $variable  variable to substitute in
-	 * @param  boolean           $and       append using and if true, or if false
-	 * @return QueryBuilderInterface         $this
-	 */
-	public function where($statement, $variable = null, $and = true);
-
-	/**
 	 * Builds into the having statement using and as default.
+	 *
 	 * @param  string | closure  $statement the statement to append
 	 * @param  mixed             $variable  variable to substitute in
 	 * @param  boolean           $and       append using and if true, or if false
+	 *
 	 * @return QueryBuilderInterface           $this
 	 */
 	public function having($statement, $variable = null, $and = true);
 
 	/**
+	 * Limits query to $limit rows
+	 *
+	 * @param  int $limit the limit
+	 *
+	 * @return QueryBuilderInterface $this
+	 */
+	public function limit($limit);
+
+	/**
 	 * Creates and returns the Query object to execute
+	 *
 	 * @return Query
 	 */
 	public function getQuery();
 
 	/**
 	 * Gets the query as an unparsed string
+	 *
 	 * @return QueryBuilderInterface $this
 	 */
 	public function getQueryString();
 
 	/**
 	 * Clears all properties
+	 *
 	 * @return QueryBuilderInterface $this
 	 */
 	public function clear();
