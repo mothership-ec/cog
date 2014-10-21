1fb1b55a633acea2ec66e5fe110cf9cf1390719e
Samuel Trangmar-Keates <sam@message.co.uk>
2014-10-21 15:41:53 +0100

    Price extension to allow default currency to be set

--- src/Templating/Twig/Extension/PriceTwigExtension.php
+++ src/Templating/Twig/Extension/PriceTwigExtension.php
@@ -4,6 +4,13 @@ namespace Message\Cog\Templating\Twig\Extension;
 
 class PriceTwigExtension extends \Twig_Extension
 {
+    private $_defaultCurrency;
+
+    public function __construct($defaultCurrency = null)
+    {
+        $this->_defaultCurrency = $defaultCurrency?:'GBP';
+    }
+
     public function getFilters()
     {
         return array(
@@ -11,14 +18,22 @@ class PriceTwigExtension extends \Twig_Extension
         );
     }
 
-    public function priceFilter($value, $currency = 'GBP', $locale = null)
+    public function priceFilter($value, $currency = null, $locale = null)
     {
  		$locale = ($locale == null ? \Locale::getDefault() : $locale);
  		$formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        $currency = $currency?:$this->_defaultCurrency;
 
         $price = $formatter->formatCurrency($value, $currency);
 
         return $price;
+    }
+
+    public function setDefaultCurrency($currency)
+    {
+        $this->_defaultCurrency = $currency;
+
+        return $this;
     }
 
     public function getName()
