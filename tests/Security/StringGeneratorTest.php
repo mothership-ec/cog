f7d76a63ca837617e6424c82abb4808633548a9f
Samuel Trangmar-Keates <sam@message.co.uk>
2015-03-04 14:10:13 +0000

    The unit tests

---
+++ tests/Security/StringGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Message\Cog\Test\Security;
+
+use Message\Cog\Security\StringGenerator;
+
+class StringGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+	private $_stringGenerator;
+
+	public function setUp()
+	{
+		$this->_stringGenerator = new StringGenerator;		
+	}
+
+	public function testGenerateDefault()
+	{
+		for ($i = 0; $i < 1000; ++$i) {
+			$string = $this->_stringGenerator->generate();
+			$this->assertEquals(strlen($string), StringGenerator::DEFAULT_LENGTH);
+		}
+	}
+
+	public function testGenerateLength()
+	{
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generate(10);
+			$this->assertEquals(strlen($string), 10);
+		}
+
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generate(50);
+			$this->assertEquals(strlen($string), 50);
+		}
+
+	}
+}
