30b6c5cdeae1b3f255677fc403ca12e170c53075
Samuel Trangmar-Keates <sam@message.co.uk>
2015-03-05 16:42:28 +0000

    Added more tests

--- tests/Security/StringGeneratorTest.php
+++ tests/Security/StringGeneratorTest.php
@@ -34,4 +34,43 @@ class StringGeneratorTest extends \PHPUnit_Framework_TestCase
 		}
 
 	}
+
+	public function testGenerateUnixLength()
+	{
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateFromUnixRandom(10);
+			$this->assertEquals(strlen($string), 10);
+		}
+
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateFromUnixRandom(50);
+			$this->assertEquals(strlen($string), 50);
+		}
+	}
+
+	public function testGenerateSSLLength()
+	{
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateFromOpenSSL(10);
+			$this->assertEquals(strlen($string), 10);
+		}
+
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateFromOpenSSL(50);
+			$this->assertEquals(strlen($string), 50);
+		}
+	}
+
+	public function testGenerateNativeLength()
+	{
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateNatively(10);
+			$this->assertEquals(strlen($string), 10);
+		}
+
+		for ($i = 0; $i < 100; ++$i) {
+			$string = $this->_stringGenerator->generateNatively(50);
+			$this->assertEquals(strlen($string), 50);
+		}
+	}
 }
