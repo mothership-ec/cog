8559c3951ab9d3a236a54d48df0409194e709e4e
thomasjthomasj <thomas@Thomass-MacBook-Pro.local>
2015-03-10 16:43:25 +0000

    swap spaces for tabs

--- src/Templating/Twig/Extension/PriceTwigExtension.php
+++ src/Templating/Twig/Extension/PriceTwigExtension.php
@@ -4,66 +4,66 @@ namespace Message\Cog\Templating\Twig\Extension;
 
 class PriceTwigExtension extends \Twig_Extension
 {
-    private $_defaultCurrency;
+	private $_defaultCurrency;
 
-    public function __construct($defaultCurrency = null)
-    {
-        $this->_defaultCurrency = $defaultCurrency?:'GBP';
-    }
+	public function __construct($defaultCurrency = null)
+	{
+		$this->_defaultCurrency = $defaultCurrency ?: 'GBP';
+	}
 
-    public function getFilters()
-    {
-        return array(
-            new \Twig_SimpleFilter('price', array($this, 'priceFilter')),
-        );
-    }
+	public function getFilters()
+	{
+		return array(
+			new \Twig_SimpleFilter('price', array($this, 'priceFilter')),
+		);
+	}
 
-    public function getFunctions()
-    {
-        return [
-            'currencySymbol' => new \Twig_Function_Method($this, 'currencySymbolFunction'),
-        ];
-    }
+	public function getFunctions()
+	{
+		return [
+			'currencySymbol' => new \Twig_Function_Method($this, 'currencySymbolFunction'),
+		];
+	}
 
-    public function priceFilter($value, $currency = null, $locale = null, array $extraOptions = [])
-    {
+	public function priceFilter($value, $currency = null, $locale = null, array $extraOptions = [])
+	{
 
-        $locale = ($locale == null ? \Locale::getDefault() : $locale);
-        $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
-        $currency = $currency?:$this->_defaultCurrency;
+		$locale = ($locale == null ? \Locale::getDefault() : $locale);
+		$formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+		$currency = $currency ?: $this->_defaultCurrency;
 
-        $price = $formatter->formatCurrency($value, $currency);
-        if (isset($extraOptions['no_decimal']) && $extraOptions['no_decimal']) {
-            $price = str_replace('.00','',$price);
-        }
+		$price = $formatter->formatCurrency($value, $currency);
+		if (isset($extraOptions['no_decimal']) && $extraOptions['no_decimal']) {
+			$price = str_replace('.00', '', $price);
+		}
 
-        return $price;
-    }
+		return $price;
+	}
 
-    /**
-     * @todo This is the easiet way I could see to get a currency from a String such as 'GBP'
-     * However it's not a very nice solution. This should probably be updated in the future.
-     */
-    public function currencySymbolFunction($currency = null, $locale= null)
-    {
-        $locale = ($locale == null ? \Locale::getDefault() : $locale);
-        $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
-        $currency = $currency?:$this->_defaultCurrency;
+	/**
+	 * @todo This is the easiet way I could see to get a currency from a String such as 'GBP'
+	 * However it's not a very nice solution. This should probably be updated in the future.
+	 */
+	public function currencySymbolFunction($currency = null, $locale = null)
+	{
+		$locale = ($locale == null ? \Locale::getDefault() : $locale);
+		$formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+		$currency = $currency ?: $this->_defaultCurrency;
 
-        $symbol = substr($formatter->formatCurrency(0, $currency), 0, -4);
+		$symbol = substr($formatter->formatCurrency(0, $currency), 0, -4);
 
-        return $symbol;
-    }
+		return $symbol;
+	}
 
-    public function setDefaultCurrency($currency)
-    {
-        $this->_defaultCurrency = $currency;
+	public function setDefaultCurrency($currency)
+	{
+		$this->_defaultCurrency = $currency;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getName()
-    {
-        return 'price_twig_extension';
-    }
+	public function getName()
+	{
+		return 'price_twig_extension';
+	}
 }
