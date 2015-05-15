12cab3e91a22f50c84d7eff50b8bf092c8c27596
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-05-15 11:49:54 +0100

    FilterCollection test

--- tests/Filter/FilterCollectionTest.php
+++ tests/Filter/FilterCollectionTest.php
@@ -2,7 +2,84 @@
 
 namespace Message\Cog\Test\Filter;
 
+use Message\Cog\Filter\FilterCollection;
+
 class FilterCollectionTest extends \PHPUnit_Framework_TestCase
 {
+	const NAME_1 = 'name_1';
+	const NAME_2 = 'name_2';
 
+	public function testConstructValid()
+	{
+		$filters = new FilterCollection([
+			new FauxFilter(self::NAME_1, self::NAME_1),
+			new FauxFilter(self::NAME_2, self::NAME_2)
+		]);
+
+		$this->assertTrue($filters->count() === 2);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testConstructInvalid()
+	{
+		$filters = new FilterCollection([
+			new \stdClass
+		]);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testConstructInvalidDuplicate()
+	{
+		$filters = new FilterCollection([
+			new FauxFilter(self::NAME_1, self::NAME_1),
+			new FauxFilter(self::NAME_1, self::NAME_1)
+		]);
+	}
+
+	public function testAdd()
+	{
+		$filters = new FilterCollection;
+
+		$filters->add(new FauxFilter(self::NAME_1, self::NAME_1));
+		$this->assertTrue($filters->count() === 1);
+
+		$filters->add(new FauxFilter(self::NAME_2, self::NAME_2));
+		$this->assertTrue($filters->count() === 2);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testAddInvalid()
+	{
+		$filters = new FilterCollection;
+
+		$filters->add(new \stdClass);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testAddInvalidDuplicate()
+	{
+		$filters = new FilterCollection;
+
+		$filters->add(new FauxFilter(self::NAME_1, self::NAME_1));
+		$filters->add(new FauxFilter(self::NAME_1, self::NAME_1));
+	}
+
+	public function testKey()
+	{
+		$filters = new FilterCollection([
+			new FauxFilter(self::NAME_1, self::NAME_1),
+		]);
+
+		foreach ($filters as $key => $value) {
+			$this->assertSame(self::NAME_1, $key);
+		}
+	}
 }
