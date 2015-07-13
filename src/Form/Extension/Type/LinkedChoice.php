86ab29c75b86214add4b3cf45e42ae3e583cb1c8
Samuel Trangmar-Keates <sam@message.co.uk>
2015-07-13 15:37:32 +0100

    Warning about array_merge

--- src/Form/Extension/Type/LinkedChoice.php
+++ src/Form/Extension/Type/LinkedChoice.php
@@ -31,7 +31,7 @@ class LinkedChoice extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		foreach ($this->_groups as $name => $choices) {
-
+			// Cannot use array-merge as we need to preserve any integer keys.
 			$choiceData = ['none' => 'None'];
 			foreach ($choices as $key => $value) {
 				$choiceData[$key] = $value;
