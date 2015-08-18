9df4fba637723670dc4613fdc9a89b1c74ebb774
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-08-18 14:31:24 +0100

    do not use strict on phpunit, and set datetime validation tests to large

--- tests/Validation/Rule/DateTest.php
+++ tests/Validation/Rule/DateTest.php
@@ -24,6 +24,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->_loader = new Loader($this->_messages, array($this->_rule));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeTrue()
 	{
 		$before = new \DateTime('01-01-1970');
@@ -31,6 +34,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($this->_rule->before($before, $after));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeFalse()
 	{
 		$before = new \DateTime('01-01-1970');
@@ -38,6 +44,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->before($after, $before));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeOrEqualToFalse()
 	{
 		$one = new \DateTime('31-03-1986');
@@ -45,6 +54,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->before($one, $two));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeOrEqualToTrue()
 	{
 		$one = new \DateTime('31-03-1986');
@@ -52,6 +64,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($this->_rule->before($one, $two, true));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeTrueWithTimeZone()
 	{
 		$uk = new \DateTime('31-03-1986', new \DateTimeZone('Europe/London'));
@@ -59,6 +74,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($this->_rule->before($uk, $usa));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testBeforeFalseWithTimeZone()
 	{
 		$uk = new \DateTime('31-03-1986', new \DateTimeZone('Europe/London'));
@@ -66,6 +84,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->before($usa, $uk));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterTrue()
 	{
 		$before = new \DateTime('01-01-1970');
@@ -73,6 +94,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($this->_rule->after($after, $before));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterFalse()
 	{
 		$before = new \DateTime('01-01-1970');
@@ -80,6 +104,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->after($before, $after));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterTrueWithTimeZone()
 	{
 		$uk = new \DateTime('31-03-1986', new \DateTimeZone('Europe/London'));
@@ -87,6 +114,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($this->_rule->after($usa, $uk));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterFalseWithTimeZone()
 	{
 		$uk = new \DateTime('31-03-1986', new \DateTimeZone('Europe/London'));
@@ -94,6 +124,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->after($uk, $usa));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterOrEqualToFalse()
 	{
 		$one = new \DateTime('31-03-1986');
@@ -101,6 +134,9 @@ class DateTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse($this->_rule->after($one, $two));
 	}
 
+	/**
+	 * @large
+	 */
 	public function testAfterOrEqualToTrue()
 	{
 		$one = new \DateTime('31-03-1986');
