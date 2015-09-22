d1584b5a61bb1969a3b6c5451bc597bb1ff66d09
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-09-22 12:38:54 +0100

    add docblock to GenerateStringException

--- src/Security/Exception/GenerateStringException.php
+++ src/Security/Exception/GenerateStringException.php
@@ -2,6 +2,14 @@
 
 namespace Message\Cog\Security\Exception;
 
+/**
+ * Class GenerateStringException
+ * @package Message\Cog\Security\Exception
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Exception to be thrown when string generator fails to generate a string matching requirements
+ */
 class GenerateStringException extends \LogicException
 {
 
