b0af70a3d74eba7e27a3f7a0b40f70be3feff91a
Samuel Trangmar-Keates <sam@message.co.uk>
2015-09-30 14:39:44 +0100

    Convert spaces to tabs in cogulerewrite filter

--- src/AssetManagement/CssCoguleRewriteFilter.php
+++ src/AssetManagement/CssCoguleRewriteFilter.php
@@ -1,4 +1,6 @@
-<?php namespace Message\Cog\AssetManagement;
+<?php
+
+namespace Message\Cog\AssetManagement;
 
 use Assetic\Asset\AssetInterface;
 use Assetic\Filter\BaseCssFilter;
@@ -11,44 +13,44 @@ class CssCoguleRewriteFilter extends BaseCssFilter
 {
 
 	public function filterLoad(AssetInterface $asset)
-    {
+	{
 
-    }
+	}
 
 	public function filterDump(AssetInterface $asset)
 	{
 		$sourceBase = $asset->getSourceRoot();
-        $sourcePath = $asset->getSourcePath();
-        $targetPath = $asset->getTargetPath();
+		$sourcePath = $asset->getSourcePath();
+		$targetPath = $asset->getTargetPath();
 
-        if (null === $sourcePath || null === $targetPath || $sourcePath == $targetPath) {
-            return;
-        }
+		if (null === $sourcePath || null === $targetPath || $sourcePath == $targetPath) {
+			return;
+		}
 
-        $cogNamespace = $asset->cogNamespace;
+		$cogNamespace = $asset->cogNamespace;
 
-        $content = $this->filterReferences($asset->getContent(), function($matches) use ($cogNamespace) {
-            if (false !== strpos($matches['url'], '://') || 0 === strpos($matches['url'], '//') || 0 === strpos($matches['url'], 'data:')) {
-                // absolute or protocol-relative or data uri
-                return $matches[0];
-            }
+		$content = $this->filterReferences($asset->getContent(), function($matches) use ($cogNamespace) {
+			if (false !== strpos($matches['url'], '://') || 0 === strpos($matches['url'], '//') || 0 === strpos($matches['url'], 'data:')) {
+				// absolute or protocol-relative or data uri
+				return $matches[0];
+			}
 
-            if (isset($matches['url'][0]) && '/' == $matches['url'][0]) {
-                // root relative
-                return $matches[0];
-            }
+			if (isset($matches['url'][0]) && '/' == $matches['url'][0]) {
+				// root relative
+				return $matches[0];
+			}
 
-            $url = $matches['url'];
-            while (0 === strpos($url, '../')) {
-                $url = substr($url, 3);
-            }
+			$url = $matches['url'];
+			while (0 === strpos($url, '../')) {
+				$url = substr($url, 3);
+			}
 
-            $url = '../../cogules/' . $cogNamespace . '/' . $url;
+			$url = '../../cogules/' . $cogNamespace . '/' . $url;
 
-        	return str_replace($matches['url'], $url, $matches[0]);
-        });
+			return str_replace($matches['url'], $url, $matches[0]);
+		});
 
-        $asset->setContent($content);
+		$asset->setContent($content);
 	}
 
 }
