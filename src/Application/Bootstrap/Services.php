4f7a1c9235a0575a7ec4b4557af85caeff4c541c
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-10-01 16:50:13 +0100

    remove unused services

--- src/Application/Bootstrap/Services.php
+++ src/Application/Bootstrap/Services.php
@@ -353,25 +353,6 @@ class Services implements ServicesInterface
 			return new \Symfony\Component\HttpKernel\UriSigner(time());
 		};
 
-		$services['http.rest.request_dispatcher_collection'] = function($c)
-		{
-			return new Cog\HTTP\REST\RequestDispatcherCollection([
-				$c['http.rest.xml_request_dispatcher'],
-			]);
-		};
-
-		$services['http.rest.xml_request_dispatcher'] = function($c)
-		{
-			return new Cog\HTTP\REST\XmlRequestDispatcher(
-				$c['http.kernel'],
-				$c['serializer.array_to_xml']
-			);
-		};
-
-		$services['http.oauth.factory'] = function($c) {
-			return new Cog\HTTP\OAuth\Factory;
-		};
-
 		$services['response_builder'] = function($c) {
 			return new Cog\Controller\ResponseBuilder(
 				$c['templating']
